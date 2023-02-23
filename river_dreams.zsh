f3d466ed4f74ac1a8d5601d3f4e65f2d49b52fbd
Sherman Rofeman <skippyr.developer@gmail.com>
2023-02-23 12:28:23 -0300

    chore: suppress errors of ip address module.

--- river_dreams.zsh
+++ river_dreams.zsh
@@ -59,8 +59,8 @@ river_dreams::clock() {
 river_dreams::ip_address() {
   local -r local_ip_address=$(
     test -x $(which ip 2>/dev/null) &>/dev/null &&
-    ip -d a | grep 'inet ' | grep -v 127.0.0 | grep -v docker | awk '{print $2}' ||
-    ifconfig | grep -A 1 RUNNING | grep 'inet ' | grep -v 127.0.0 | awk '{print $2}'
+    ip -d a 2>/dev/null | grep 'inet ' | grep -v 127.0.0 | grep -v docker | awk '{print $2}' ||
+    ifconfig 2>/dev/null | grep -A 1 RUNNING | grep 'inet ' | grep -v 127.0.0 | awk '{print $2}'
   )
   if [[ -n ${local_ip_address} ]]; then
     echo "%F{blue} %F{normal}${local_ip_address}"
