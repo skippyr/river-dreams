8fd35ec14abab99a73e0c5af47e2ee151c8e169b
Sherman Rofeman <skippyr.developer@gmail.com>
2023-02-24 16:20:08 -0300

    refactor: make clock module configurable.

--- modules/clock.zsh
+++ modules/clock.zsh
@@ -1,6 +1,13 @@
 #!/usr/bin/env zsh
 
+river_dreams::clock::get_current_hours_and_minutes() {
+  date +%Hh%Mm
+}
+
 river_dreams::clock() {
-  local -r clock=$(date +%Hh%Mm)
-  echo "%F{yellow} %F{normal}${clock}"
+  local -r clock_symbol=${RIVER_DREAMS_CLOCK_SYMBOL:- }
+  local -r clock_symbol_color=${RIVER_DREAMS_CLOCK_SYMBOL_COLOR:-yellow}
+
+  local -r current_hours_and_minutes=$(river_dreams::clock::get_current_hours_and_minutes)
+  echo "%F{${clock_symbol_color}}${clock_symbol}%F{normal}${current_hours_and_minutes}"
 }
