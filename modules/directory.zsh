fc5105fc68cf0776776e043ad17e42f37fe6200d
Sherman Rofeman <skippyr.developer@gmail.com>
2023-02-24 16:40:18 -0300

    refactor: make directory module configurable.

--- modules/directory.zsh
+++ modules/directory.zsh
@@ -1,27 +1,46 @@
 #!/usr/bin/env zsh
 
-river_dreams::directory() {
+river_dreams::directory::get_directory_symbol() {
+  local -r home_symbol=${RIVER_DREAMS_DIRECTORY_HOME_SYMBOL:- }
+  local -r downloads_symbol=${RIVER_DREAMS_DIRECTORY_DOWNLOADS_SYMBOL:- }
+  local -r documents_symbol=${RIVER_DREAMS_DIRECTORY_DOCUMENTS_SYMBOL:- }
+  local -r pictures_symbol=${RIVER_DREAMS_DIRECTORY_PICTURES_SYMBOL:-🖼 }
+  local -r music_symbol=${RIVER_DREAMS_DIRECTORY_MUSIC_SYMBOL:-🎜 }
+  local -r public_symbol=${RIVER_DREAMS_DIRECTORY_PUBLIC_SYMBOL:- }
+  local -r videos_symbol=${RIVER_DREAMS_DIRECTORY_VIDEOS_SYMBOL:- }
+  local -r templates_symbol=${RIVER_DREAMS_DIRECTORY_TEMPLATES_SYMBOL:- }
+  local -r config_symbol=${RIVER_DREAMS_DIRECTORY_CONFIG_SYMBOL:- }
+  local -r cache_symbol=${RIVER_DREAMS_DIRECTORY_CACHE_SYMBOL:- }
+
   local -r current_directory=$(pwd)
-  local directory_icon=""
+
   [[ ${current_directory} == ${HOME} ]] &&
-    directory_icon=" "
+    echo ${home_symbol}
   [[ ${current_directory} == ${HOME}/Downloads ]] &&
-    directory_icon=" "
+    echo ${downloads_symbol}
   [[ ${current_directory} == ${HOME}/Documents ]] &&
-    directory_icon=" "
+    echo ${documents_symbol}
   [[ ${current_directory} == ${HOME}/Pictures ]] &&
-    directory_icon="🖼 "
+    echo ${pictures_symbol}
   [[ ${current_directory} == ${HOME}/Music ]] &&
-    directory_icon="🎜 "
+    echo ${music_symbol}
   [[ ${current_directory} == ${HOME}/Public ]] &&
-    directory_icon=" "
+    echo ${public_symbol}
   [[ ${current_directory} == ${HOME}/Videos ]] &&
-    directory_icon=" "
+    echo ${videos_symbol}
   [[ ${current_directory} == ${HOME}/Templates ]] &&
-    directory_icon=" "
+    echo ${templates_symbol}
   [[ ${current_directory} == ${HOME}/.config ]] &&
-    directory_icon=" "
+    echo ${config_symbol}
   [[ ${current_directory} == ${HOME}/.cache ]] &&
-    directory_icon=" "
-  echo "%F{yellow}⤐ ${directory_icon}%F{green}%B%1~%b"
+    echo ${cache_symbol}
+}
+
+river_dreams::directory() {
+  local -r directory_color=${RIVER_DREAMS_DIRECTORY_DIRECTORY_COLOR:-green}
+  local -r directory_symbol_color=${RIVER_DREAMS_DIRECTORY_SYMBOL_COLOR:-yellow}
+  
+  local -r directory_symbol=$(river_dreams::directory::get_directory_symbol)
+
+  echo "%F{${directory_symbol_color}}${directory_symbol}%F{${directory_color}}%B%1~%b"
 }
