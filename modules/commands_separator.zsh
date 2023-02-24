b1e335bcff1297958f9507a63fe50cb87caddc30
Sherman Rofeman <skippyr.developer@gmail.com>
2023-02-24 16:22:04 -0300

    refactor: make commands separator module configurable.

--- modules/commands_separator.zsh
+++ modules/commands_separator.zsh
@@ -1,7 +1,9 @@
 #!/usr/bin/env zsh
 
 river_dreams::commands_separator() {
+  local -r commands_separator_symbol=${RIVER_DREAMS_COMMANDS_SEPARATOR_SYMBOL:-▽}
+  
   for column in $(seq 1 ${COLUMNS}); do
-    echo -n "▽"
+    echo -n ${commands_separator_symbol}
   done
 }
