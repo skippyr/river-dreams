4b72f63ce01c5b2d3b36a03e950cde5abf6afb2d
Sherman Rofeman <skippyr.developer@gmail.com>
2023-02-24 18:01:14 -0300

    refactor: make hidden files configurable.

--- modules/hidden_files.zsh
+++ modules/hidden_files.zsh
@@ -1,8 +1,16 @@
 #!/usr/bin/env zsh
 
+river_dreams::get_hidden_files_quantity() {
+  ls -d .* 2>/dev/null | wc -l
+}
+
 river_dreams::hidden_files() {
-  local -r hidden_files_quantity=$(ls -d .* 2>/dev/null | wc -l)
+  local -r hidden_files_symbol=${RIVER_DREAMS_HIDDEN_FILES_SYMBOL:- }
+  local -r hidden_files_quantity_color=${RIVER_DREAMS_HIDDEN_FILES_SYMBOL_COLOR:-red}
+
+  local -r hidden_files_quantity=$(river_dreams::get_hidden_files_quantity)
+
   if [[ ${hidden_files_quantity} -gt 0 ]]; then
-    echo "%F{red} %F{normal}${hidden_files_quantity}"
+    echo "%F{${hidden_files_quantity_color}}${hidden_files_symbol}%F{normal}${hidden_files_quantity}"
   fi
 }
