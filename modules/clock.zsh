077707f657f30e382e5f1e7fff589029c44aa0e1
Sherman Rofeman <skippyr.developer@gmail.com>
2023-02-26 11:04:43 -0300

    feat: add extra symbol to clock module.

--- modules/clock.zsh
+++ modules/clock.zsh
@@ -1,5 +1,23 @@
 #!/usr/bin/env zsh
 
 river_dreams::clock() {
-  echo "%F{yellow} %F{normal}$(date +%Hh%Mm)"
+  local symbol=""
+  local symbol_color=""
+  local current_hours=$(date +%_H)
+
+  if [[ ${current_hours} -lt 6 ]]; then
+    symbol="✪"
+    symbol_color="blue"
+  elif [[ ${current_hours} -lt 12 ]]; then
+    symbol=""
+    symbol_color="yellow"
+  elif [[ ${current_hours} -lt 18 ]]; then
+    symbol=""
+    symbol_color="yellow"
+  else
+    symbol="⏾"
+    symbol_color="blue"
+  fi
+
+  echo "%F{yellow} %F{${symbol_color}}${symbol} %F{normal}$(date +%Hh%Mm)"
 }
