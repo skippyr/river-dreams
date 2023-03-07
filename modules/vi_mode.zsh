fb916530f8c69995664631135e85efaa56e7a008
Sherman Rofeman <skippyr.developer@gmail.com>
2023-03-07 12:46:26 -0300

    feat: add git hash in git module.

--- modules/vi_mode.zsh
+++ modules/vi_mode.zsh
@@ -1,13 +1,13 @@
 #!/usr/bin/env zsh
 
 function zle-line-init zle-keymap-select {
-    export RIVER_DREAMS_MODE_SYMBOL="%F{red}?"
+    export RIVER_DREAMS_VI_MODE_SYMBOL="%F{red}?"
     case ${KEYMAP} in
       main)
-        export RIVER_DREAMS_MODE_SYMBOL="%F{green}I"
+        export RIVER_DREAMS_VI_MODE_SYMBOL="%F{green}I"
         ;;
       vicmd)
-        export RIVER_DREAMS_MODE_SYMBOL="%F{red}N"
+        export RIVER_DREAMS_VI_MODE_SYMBOL="%F{red}N"
         ;;
     esac
     zle reset-prompt
@@ -17,6 +17,6 @@ zle -N zle-line-init
 zle -N zle-keymap-select
 
 river_dreams::vi_mode() {
-  echo "%F{yellow}[%B${RIVER_DREAMS_MODE_SYMBOL}%b%F{yellow}]"
+  echo "%F{yellow}[%B${RIVER_DREAMS_VI_MODE_SYMBOL}%b%F{yellow}]"
 }
 
