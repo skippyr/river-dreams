68b52b4b9456729f383f7357b4614048764ac79d
Sherman Rofeman <skippyr.developer@gmail.com>
2023-03-08 12:37:46 -0300

    refactor: change spacing order in decorator module

--- modules/decorator.zsh
+++ modules/decorator.zsh
@@ -3,8 +3,8 @@
 river_dreams::decorator() {
   local -r decorator_symbol=$(
     test $(tput colors) -eq 8 &&
-    echo ">=>> " ||
-    echo "⤐ "
+    echo ">=>>" ||
+    echo "⤐"
   )
-  echo "%(?.%F{green}.%F{red})${decorator_symbol}%f"
+  echo "%(?.%F{green}.%F{red})${decorator_symbol} %f"
 }
