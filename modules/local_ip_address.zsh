2c411e8106fd2daf895f4e7675e66d937517c951
Sherman Rofeman <skippyr.developer@gmail.com>
2023-03-08 23:53:00 -0300

    refactor: change wrong function name

--- modules/local_ip_address.zsh
+++ modules/local_ip_address.zsh
@@ -4,14 +4,14 @@ river_dreams::local_ip_address::get_local_ip_address_using_ip() {
   ip -d a 2>/dev/null | grep 'inet ' | grep -v 127.0.0 | grep -v docker | awk '{print $2}'
 }
 
-river_dreams::local_ip_address::get_local_ip_address_using_ipconfig() {
+river_dreams::local_ip_address::get_local_ip_address_using_ifconfig() {
   ifconfig 2>/dev/null | grep -A 1 RUNNING | grep 'inet ' | grep -v 127.0.0 | awk '{print $2}'
 }
 
 river_dreams::ip_address::get_local_ip_address() {
   test -x $(which ip 2>/dev/null) &>/dev/null &&
   river_dreams::local_ip_address::get_local_ip_address_using_ip ||
-  river_dreams::local_ip_address::get_local_ip_address_using_ipconfig
+  river_dreams::local_ip_address::get_local_ip_address_using_ifconfig
 }
 
 river_dreams::local_ip_address() {
