c0b29a1da69840ef11f8e4c047988625b99f21dd
Sherman Rofeman <skippyr.developer@gmail.com>
2023-03-11 13:33:53 -0300

    refactor: add check to storage devices module

--- modules/storage_devices.zsh
+++ modules/storage_devices.zsh
@@ -1,11 +1,6 @@
 #!/usr/bin/env zsh
 
 river_dreams::storage_devices() {
-  local -r storage_devices_symbol=$(
-    test ${RIVER_DREAMS_USE_FALLBACK_TEXT} == true &&
-    echo "DEVICES" ||
-    echo "󱊞"
-  )
   local -r system_storage_device=$(
     lsblk -l |
     grep /$ |
@@ -19,6 +14,9 @@ river_dreams::storage_devices() {
     awk '{print $1}' |
     grep -v ${system_storage_device}
   ))
+
+  [[ ${#external_storage_devices[@]} -eq 0 ]] && exit
+
   local mounted_external_storage_devices_quantity=0
 
   for external_storage_device in ${external_storage_devices[@]}; do
@@ -30,8 +28,11 @@ river_dreams::storage_devices() {
   [[ ${mounted_external_storage_devices_quantity} -gt 0 ]] &&
     mount_section=" [%F{yellow}${mounted_external_storage_devices_quantity}%f]"
 
-  if [[ ${#external_storage_devices[@]} -gt 0 ]]; then
-    echo "%F{yellow}${storage_devices_symbol} %f${#external_storage_devices[@]}${mount_section}%f"
-  fi
-}
+  local -r storage_devices_symbol=$(
+    [[ ${RIVER_DREAMS_USE_FALLBACK_TEXT} == true ]] &&
+    echo "DEVICES" ||
+    echo "󱊞"
+  )
 
+  echo "%F{yellow}${storage_devices_symbol} %f${#external_storage_devices[@]}${mount_section}%f"
+}
