bffa9597efd98bda38bb9bc5bec6bec0467b986e
Sherman Rofeman <skippyr.developer@gmail.com>
2023-03-11 12:14:55 -0300

    refactor: remove unecessary parse

--- modules/git.zsh
+++ modules/git.zsh
@@ -1,47 +1,47 @@
 #!/usr/bin/env zsh
 
 river_dreams::git::get_branch() {
-  local git_short_status="$1"
-  local branch_line=$(echo "${git_short_status}" | head -n 1)
+  local -r branch_line="$1"
   [[ $(echo "${branch_line}" | grep "\.") ]] &&
     echo "${branch_line}" | cut -f 2 -d " " | cut -f 1 -d "." ||
     echo "${branch_line}" | rev | cut -f 1 -d " " | rev
 }
 
 river_dreams::git::get_changes_quantity() {
-  local git_short_status="$1"
+  local -r git_short_status="$1"
   echo "${git_short_status}" | grep "[?NMDT] \S" | wc -l
 }
 
 river_dreams::git::get_commits_quantity() {
-  local git_short_status="$1"
+  local -r git_short_status="$1"
   echo "${git_short_status}" | grep "^[ANMDRUT].* " | wc -l
 }
 
 river_dreams::git::get_pushes_quantity() {
-  local git_short_status="$1"
-  echo  "${git_short_status}" | grep "#" | cut -f 4 -d " " | tr -d "]"
+  local -r branch_line="$1"
+  echo  "${branch_line}" | cut -f 4 -d " " | tr -d "]"
 }
 
 river_dreams::git() {
   local git_short_status=$(git status -sb 2>/dev/null)
-  local -r branch=$(river_dreams::git::get_branch "${git_short_status}")
+  local -r branch_line=$(echo "${git_short_status}" | head -n 1)
+  local -r branch=$(river_dreams::git::get_branch "${branch_line}")
 
   [[ -z ${branch} ]] && exit
 
   local -r changes_quantity=$(river_dreams::git::get_changes_quantity "${git_short_status}")
   local -r changes_section=$(
-    test ${changes_quantity} -gt 0 &&
+    [[ ${changes_quantity} -gt 0 ]] &&
     echo "${changes_quantity}* " ||
     echo ""
   )
   local -r commits_quantity=$(river_dreams::git::get_commits_quantity "${git_short_status}")
   local -r commit_section=$(
-    test ${commits_quantity} -gt 0 &&
+    [[ ${commits_quantity} -gt 0 ]] &&
     echo "${commits_quantity}+ " ||
     echo ""
   )
-  local -r pushes_quantity=$(river_dreams::git::get_pushes_quantity "${git_short_status}")
+  local -r pushes_quantity=$(river_dreams::git::get_pushes_quantity "${branch_line}")
   local -r push_section=$(
     [[ ${pushes_quantity} -gt 0 ]] &&
     echo "${pushes_quantity}↑ " ||
