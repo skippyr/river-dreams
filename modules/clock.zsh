5ae0a4a7c4ca0ec1e20d93f67fd5af7a0679808b
Sherman Rofeman <skippyr.developer@gmail.com>
2023-03-11 13:25:02 -0300

    refactor: remove unecessary variable from clock module

--- modules/clock.zsh
+++ modules/clock.zsh
@@ -1,29 +1,28 @@
 #!/usr/bin/env zsh
 
 river_dreams::clock() {
-  local symbol=""
   local clock_symbol=""
-  local symbol_color=""
+  local clock_symbol_color=""
   local -r current_hours=$(date +%_H)
 
   if [[ ${RIVER_DREAMS_USE_FALLBACK_TEXT} == true ]]; then
-    symbol="CLOCK"
-    symbol_color="yellow"
+    clock_symbol="CLOCK"
+    clock_symbol_color="yellow"
   else
     if [[ ${current_hours} -lt 6 ]]; then
-      symbol=""
-      symbol_color="magenta"
+      clock_symbol=""
+      clock_symbol_color="magenta"
     elif [[ ${current_hours} -lt 12 ]]; then
-      symbol=""
-      symbol_color="yellow"
+      clock_symbol=""
+      clock_symbol_color="yellow"
     elif [[ ${current_hours} -lt 18 ]]; then
-      symbol="󰖚"
-      symbol_color="red"
+      clock_symbol="󰖚"
+      clock_symbol_color="red"
     else
-      symbol="󰽥"
-      symbol_color="yellow"
+      clock_symbol="󰽥"
+      clock_symbol_color="yellow"
     fi
   fi
 
-  echo "%F{${symbol_color}}${symbol} %f$(date +%Hh%Mm)"
+  echo "%F{${clock_symbol_color}}${clock_symbol} %f$(date +%Hh%Mm)"
 }
