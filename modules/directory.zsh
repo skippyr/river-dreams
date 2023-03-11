3b68c6efb63bc13be915f144be96d1d4f6b0511f
Sherman Rofeman <skippyr.developer@gmail.com>
2023-03-11 10:00:27 -0300

    refactor: make directory use case instead of if clauses

--- modules/directory.zsh
+++ modules/directory.zsh
@@ -1,31 +1,40 @@
 #!/usr/bin/env zsh
 
 river_dreams::directory() {
-  local -r current_directory=$(pwd)
   local directory_symbol=""
-
   if [[ ! ${RIVER_DREAMS_USE_FALLBACK_TEXT} == true ]]; then
-    if [[ ${current_directory} == ${HOME} ]]; then
-      directory_symbol=" "
-    elif [[ ${current_directory} == ${HOME}/Downloads ]]; then
-      directory_symbol=" "
-    elif [[ ${current_directory} == ${HOME}/Documents ]]; then
-      directory_symbol=" "
-    elif [[ ${current_directory} == ${HOME}/Pictures ]]; then
-      directory_symbol=" "
-    elif [[ ${current_directory} == ${HOME}/Music ]]; then
-      directory_symbol="🎜 "
-    elif [[ ${current_directory} == ${HOME}/Public ]]; then
-      directory_symbol=" "
-    elif [[ ${current_directory} == ${HOME}/Videos ]]; then
-      directory_symbol="󰨜 "
-    elif [[ ${current_directory} == ${HOME}/Templates ]]; then
-      directory_symbol=" "
-    elif [[ ${current_directory} == ${HOME}/.config ]]; then
-      directory_symbol=" "
-    elif [[ ${current_directory} == ${HOME}/.cache ]]; then
-      directory_symbol=" "
-    fi
+    case $(pwd) in
+      ${HOME})
+        directory_symbol=" "
+        ;;
+      ${HOME}/Downloads)
+        directory_symbol=" "
+        ;;
+      ${HOME}/Documents)
+        directory_symbol=" "
+        ;;
+      ${HOME}/Pictures)
+        directory_symbol=" "
+        ;;
+      ${HOME}/Music)
+        directory_symbol="🎜 "
+        ;;
+      ${HOME}/Public)
+        directory_symbol=" "
+        ;;
+      ${HOME}/Vides)
+        directory_symbol="󰨜 "
+        ;;
+      ${HOME}/Templates)
+        directory_symbol=" "
+        ;;
+      ${HOME}/.config)
+        directory_symbol=" "
+        ;;
+      ${HOME}/.cache)
+        directory_symbol=" "
+        ;;
+    esac
   fi
   echo "%F{red}${directory_symbol}%F{cyan}%B%1~%b%f"
 }
--- river_dreams.zsh-theme
+++ river_dreams.zsh-theme
@@ -6,7 +6,6 @@ setopt +o nomatch
 export VIRTUAL_ENV_DISABLE_PROMPT=1
 export RIVER_DREAMS_MODULES_DIRECTORY=$(dirname $0)/modules
 export RIVER_DREAMS_PROMPT_COMPONENTS_DIRECTORY=$(dirname $0)/prompt_components
-export RIVER_DREAMS_CONFIGURATIONS_FILE=~/.config/river_dreams/configurations.zsh
 export RIVER_DREAMS_USE_FALLBACK_TEXT=${RIVER_DREAMS_USE_FALLBACK_TEXT:-$(
   test $(tput colors) -eq 8 &&
   echo "true" ||
@@ -20,8 +19,6 @@ done
 for prompt_component_file in $(ls ${RIVER_DREAMS_PROMPT_COMPONENTS_DIRECTORY}); do
   source ${RIVER_DREAMS_PROMPT_COMPONENTS_DIRECTORY}/${prompt_component_file}
 done
-
-source ${RIVER_DREAMS_CONFIGURATIONS_FILE} 2>/dev/null
 
 PROMPT='$(river_dreams::commands_separator)$(river_dreams::top_prompt_component)
 $(river_dreams::left_prompt_component)%f '
