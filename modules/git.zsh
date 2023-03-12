727b273330e9f550fc06cc7025c85579e7136121
Sherman Rofeman <skippyr.developer@gmail.com>
2023-03-12 12:58:10 -0300

    refactor: add new function to get changes quantity

--- modules/git.zsh
+++ modules/git.zsh
@@ -8,10 +8,18 @@ river_dreams::git::get_commit_hash() {
   git rev-parse --short HEAD 2>/dev/null
 }
 
+river_dreams::git::get_changes_quantity() {
+  git ls-files -mo | wc -l
+}
+
 river_dreams::git() {
   local -r branch=$(river_dreams::git::get_branch)
   [[ -z ${branch} ]] && exit
   local commit_hash=$(river_dreams::git::get_commit_hash)
   [[ -n ${commit_hash} ]] && commit_hash=" ${commit_hash}"
-  echo "git:(${branch}${commit_hash})"
+  local changes_quantity=$(river_dreams::git::get_changes_quantity)
+  local changes_section=""
+  [[ ${changes_quantity} -gt 0 ]] && changes_section="${changes_quantity}* "
+  echo "%F{red}«%F{yellow}${changes_section}%f${branch}%F{yellow}${commit_hash}%F{red}»"
 }
+
