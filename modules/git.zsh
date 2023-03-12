a1ec706b2da80a68c71f53bcbbbf51396f64dd7f
Sherman Rofeman <skippyr.developer@gmail.com>
2023-03-12 11:32:46 -0300

    refactor: add new function to get branch name

--- modules/git.zsh
+++ modules/git.zsh
@@ -1,54 +1,11 @@
 #!/usr/bin/env zsh
 
 river_dreams::git::get_branch() {
-  local -r branch_line="$1"
-  [[ $(echo "${branch_line}" | grep "\.") ]] &&
-    echo "${branch_line}" | cut -f 2 -d " " | cut -f 1 -d "." ||
-    echo "${branch_line}" | rev | cut -f 1 -d " " | rev
-}
-
-river_dreams::git::get_changes_quantity() {
-  local -r git_short_status="$1"
-  echo "${git_short_status}" | grep "[?NMDT] \S" | wc -l
-}
-
-river_dreams::git::get_commits_quantity() {
-  local -r git_short_status="$1"
-  echo "${git_short_status}" | grep "^[ANMDRUT].* " | wc -l
-}
-
-river_dreams::git::get_pushes_quantity() {
-  local -r branch_line="$1"
-  echo  "${branch_line}" | cut -f 4 -d " " | tr -d "]"
+  git branch --show-current 2>/dev/null
 }
 
 river_dreams::git() {
-  local -r git_short_status=$(git status -sb 2>/dev/null)
-  local -r branch_line=$(echo "${git_short_status}" | head -n 1)
-  local -r branch=$(river_dreams::git::get_branch "${branch_line}")
-
+  local -r branch=$(river_dreams::git::get_branch)
   [[ -z ${branch} ]] && exit
-
-  local -r changes_quantity=$(river_dreams::git::get_changes_quantity "${git_short_status}")
-  local -r changes_section=$(
-    [[ ${changes_quantity} -gt 0 ]] &&
-    echo "${changes_quantity}* " ||
-    echo ""
-  )
-  local -r commits_quantity=$(river_dreams::git::get_commits_quantity "${git_short_status}")
-  local -r commit_section=$(
-    [[ ${commits_quantity} -gt 0 ]] &&
-    echo "${commits_quantity}+ " ||
-    echo ""
-  )
-  local -r pushes_quantity=$(river_dreams::git::get_pushes_quantity "${branch_line}")
-  local -r push_section=$(
-    [[ ${pushes_quantity} -gt 0 ]] &&
-    echo "${pushes_quantity}↑ " ||
-    echo ""
-  )
-  local last_commit_hash=$(git log -1 --pretty=oneline 2>/dev/null | cut -c -8)
-  [[ -n ${last_commit_hash} ]] && last_commit_hash=" ${last_commit_hash}"
-
-  echo "%F{red}«%F{red}${changes_section}%F{green}${commit_section}%F{yellow}${push_section}%f${branch}%F{yellow}${last_commit_hash}%F{red}»%f"
+  echo "git:(${branch})"
 }
--- prompt_components/top_prompt_component.zsh
+++ prompt_components/top_prompt_component.zsh
@@ -3,12 +3,12 @@
 river_dreams::top_prompt_component() {
   local top_prompt_components=()
 
-  top_prompt_components+=($(river_dreams::local_ip_address))
-  top_prompt_components+=($(river_dreams::clock))
-  top_prompt_components+=($(river_dreams::disk_usage))
-  top_prompt_components+=($(river_dreams::python_environment))
-  top_prompt_components+=($(river_dreams::docker_containers))
-  top_prompt_components+=($(river_dreams::storage_devices))
+  # top_prompt_components+=($(river_dreams::local_ip_address))
+  # top_prompt_components+=($(river_dreams::clock))
+  # top_prompt_components+=($(river_dreams::disk_usage))
+  # top_prompt_components+=($(river_dreams::python_environment))
+  # top_prompt_components+=($(river_dreams::docker_containers))
+  # top_prompt_components+=($(river_dreams::storage_devices))
 
   echo ${top_prompt_components[@]}
 }
