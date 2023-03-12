05eed6e8dbdf79007adc055db6a6fab4da4a0383
Sherman Rofeman <skippyr.developer@gmail.com>
2023-03-12 12:31:10 -0300

    refactor: add new function to get git commit hash

--- modules/git.zsh
+++ modules/git.zsh
@@ -4,8 +4,14 @@ river_dreams::git::get_branch() {
   git branch --show-current 2>/dev/null
 }
 
+river_dreams::git::get_commit_hash() {
+  git rev-parse --short HEAD 2>/dev/null
+}
+
 river_dreams::git() {
   local -r branch=$(river_dreams::git::get_branch)
   [[ -z ${branch} ]] && exit
-  echo "git:(${branch})"
+  local commit_hash=$(river_dreams::git::get_commit_hash)
+  [[ -n ${commit_hash} ]] && commit_hash=" ${commit_hash}"
+  echo "git:(${branch}${commit_hash})"
 }
