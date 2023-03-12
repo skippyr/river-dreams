e3b4c88a6524153194b5154f2c5c5a49fcf64e77
Sherman Rofeman <skippyr.developer@gmail.com>
2023-03-12 13:40:16 -0300

    refactor: add new function to get files to be pushed to upstream

--- modules/git.zsh
+++ modules/git.zsh
@@ -12,6 +12,14 @@ river_dreams::git::get_changes_quantity() {
   git ls-files -mo | wc -l
 }
 
+river_dreams::git::get_staged_quantity() {
+  git diff --name-only --cached | wc -l
+}
+
+river_dreams::git::get_pushes_quantity() {
+  git cherry 2>/dev/null | wc -l
+}
+
 river_dreams::git() {
   local -r branch=$(river_dreams::git::get_branch)
   [[ -z ${branch} ]] && exit
@@ -20,6 +28,11 @@ river_dreams::git() {
   local changes_quantity=$(river_dreams::git::get_changes_quantity)
   local changes_section=""
   [[ ${changes_quantity} -gt 0 ]] && changes_section="${changes_quantity}* "
-  echo "%F{red}«%F{yellow}${changes_section}%f${branch}%F{yellow}${commit_hash}%F{red}»"
+  local staged_quantity=$(river_dreams::git::get_staged_quantity)
+  local staged_section=""
+  [[ ${staged_quantity} -gt 0 ]] && staged_section="${staged_quantity}+ "
+  local pushes_quantity=$(river_dreams::git::get_pushes_quantity)
+  local pushes_section=""
+  [[ ${pushes_quantity} -gt 0 ]] && pushes_section="${pushes_quantity}↑ "
+  echo "%F{red}«%F{yellow}${changes_section}%F{green}${staged_section}%F{blue}${pushes_section}%f${branch}%F{yellow}${commit_hash}%F{red}»"
 }
-
