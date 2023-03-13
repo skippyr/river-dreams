2feb072602a4e376a2856010ee186fc0b2220826
Sherman Rofeman <skippyr.developer@gmail.com>
2023-03-13 18:09:23 -0300

    fix: add --exclude-standard flag when indexing changes quantity (#12)

--- modules/git.zsh
+++ modules/git.zsh
@@ -9,7 +9,7 @@ river_dreams::git::get_commit_hash() {
 }
 
 river_dreams::git::get_changes_quantity() {
-  git ls-files -m | wc -l
+  git ls-files -mo --exclude-standard | wc -l
 }
 
 river_dreams::git::get_staged_quantity() {
