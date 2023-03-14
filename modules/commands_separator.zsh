0a1a16463bf0de6b22f19f8ea879bd4cd9170e39
Sherman Rofeman <skippyr.developer@gmail.com>
2023-03-14 08:52:27 -0300

    refactor: change commands separator symbols

--- modules/commands_separator.zsh
+++ modules/commands_separator.zsh
@@ -1,10 +1,15 @@
 #!/usr/bin/env zsh
 
 river_dreams::commands_separator() {
-  local commands_separator_symbol=▽
-  [[ ${RIVER_DREAMS_USE_FALLBACK_TEXT} == true ]] &&
-    commands_separator_symbol=v
+  local commands_separator_symbol_even="⪘"
+  local commands_separator_symbol_odd="⩿"
+  if [[ ${RIVER_DREAMS_USE_FALLBACK_TEXT} == true ]]; then
+    local commands_separator_symbol_even=">"
+    local commands_separator_symbol_odd="<"
+  fi
   for column in $(seq 1 ${COLUMNS}); do
-    echo -n ${commands_separator_symbol}
+    [[ $((${column} % 2 )) -eq 0 ]] &&
+      echo -n "${commands_separator_symbol_even}" ||
+      echo -n "${commands_separator_symbol_odd}" ||
   done
 }
