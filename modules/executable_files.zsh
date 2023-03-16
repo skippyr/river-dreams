dcb5d1671780de9e3288a080a12a6bc1f03cdac5
Sherman Rofeman <skippyr.developer@gmail.com>
2023-03-16 19:12:55 -0300

    chore: change executable_files symbol

--- modules/executable_files.zsh
+++ modules/executable_files.zsh
@@ -9,7 +9,7 @@ river_dreams::executable_files() {
   local -r executable_files_symbol=$(
     [[ ${RIVER_DREAMS_USE_FALLBACK_TEXT} == true ]] &&
       echo "EXECUTABLE" ||
-      echo ""
+      echo ""
   )
   echo "%F{green}${executable_files_symbol} %f${executable_files_quantity}"
 }
