0d512f901f41c15e9311eb3fc58937f5de7c5ca2
Sherman Rofeman <skippyr.developer@gmail.com>
2023-03-16 15:11:22 -0300

    chore: remove unused prompt_components directory

--- prompt_components/left_prompt_component.zsh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env zsh
-
-river_dreams::left_prompt_component() {
-  local ligature_symbol="╚"
-  [[ ${RIVER_DREAMS_USE_FALLBACK_TEXT} == true ]] && ligature_symbol="└"
-  echo %F{red}${ligature_symbol}%f$(river_dreams::exit_code)$(river_dreams::root)$(river_dreams::vi_mode)$(river_dreams::decorator)$(river_dreams::directory)$(river_dreams::git)$(river_dreams::directory_ownership)
-}
