e08eaad75a1d8afba464cd861859593aa980994f
Sherman Rofeman <skippyr.developer@gmail.com>
2023-03-19 19:17:40 -0300

    fix: make ignored files be considered even in parent directories

--- modules/ignored_files.zsh
+++ modules/ignored_files.zsh
@@ -2,13 +2,15 @@
 
 river_dreams::ignored_files() {
   local -r ignored_files_quantity=$(
-    git clean -ndX 2>/dev/null | wc -l
+    git status -sb --ignored 2>/dev/null | grep "! " | wc -l
   )
-  [[ ${ignored_files_quantity} -eq 0 ]] && exit
-  local -r ignored_files_symbol=$(
-    [[ ${RIVER_DREAMS_USE_FALLBACK_TEXT} == true ]] &&
-    echo "IGNORED" ||
-    echo "󰮁"
-  )
-  echo "%F{magenta}${ignored_files_symbol} %f${ignored_files_quantity}"
+  if [[ ${ignored_files_quantity} -gt 0 ]]; then
+    local -r ignored_files_symbol=$(
+      [[ ${RIVER_DREAMS_USE_FALLBACK_TEXT} == true ]] &&
+      echo "IGNORED" ||
+      echo "󰮁"
+    )
+    echo "%F{magenta}${ignored_files_symbol} %f${ignored_files_quantity}"
+  fi
 }
+
