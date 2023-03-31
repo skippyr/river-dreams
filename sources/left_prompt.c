713972e032d3d82efc4b2160708dd5ab998eaa47
Sherman Rofeman <skippyr.developer@gmail.com>
2023-03-31 14:08:00 +0000

    refactor: move specif functions to left_prompt.c instead of lib.c

--- sources/left_prompt.c
+++ sources/left_prompt.c
@@ -8,6 +8,59 @@
 #include <sys/statvfs.h>
 #include "lib.c"
 
+int
+has_ownership(char* path)
+{
+  unsigned int user_uid = getuid();
+  struct stat status;
+  stat(path, &status);
+  if (status.st_uid == user_uid || user_uid == 0) {
+    return 1;
+  }
+  return 0;
+}
+
+void
+get_directory_path_abbreviated(
+  char *directory_path,
+  char *directory_path_abbreviated
+)
+{
+  int path_slice_last_index = 0;
+  for (int i = 0; i != strlen(directory_path); ++i) {
+    if (directory_path[i] == '/' && i != 0) {
+      ++path_slice_last_index;
+    }
+  }
+  int directory_path_abbreviated_index = 0;
+  int path_slice_index = 0;
+  for (int i = 0; i != strlen(directory_path); ++i) {
+    if (directory_path[i] == '/' && i != 0) {
+      ++path_slice_index;
+    }
+    if (
+      path_slice_index == path_slice_last_index ||
+      directory_path[i] == '/' ||
+      directory_path[i] == '.' ||
+      directory_path[i] == '~'
+    ) {
+      *(directory_path_abbreviated + directory_path_abbreviated_index) =
+        directory_path[i];
+      ++directory_path_abbreviated_index;
+    } else {
+      if (directory_path[i - 1] == '/') {
+        *(directory_path_abbreviated + directory_path_abbreviated_index) =
+          directory_path[i];
+        ++directory_path_abbreviated_index;
+      } else if (directory_path[i - 2] == '/' && directory_path[i - 1] == '.') {
+        *(directory_path_abbreviated + directory_path_abbreviated_index) =
+          directory_path[i];
+        ++directory_path_abbreviated_index;
+      }
+    }
+  }
+}
+
 void
 print_separator()
 {
