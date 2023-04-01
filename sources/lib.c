48dfbe6fcb87328b9f8d4b5c14b378f6822f643d
Sherman Rofeman <skippyr.developer@gmail.com>
2023-04-01 12:41:17 -0300

    refactor: substitute char * type for const char *

--- sources/lib.c
+++ sources/lib.c
@@ -1,16 +1,16 @@
 #define ENV_FALLBACK_TEXT_KEY "RIVER_DREAMS_USE_FALLBACK_TEXT"
 #define ENV_COLOR_VARIANTS_KEY "RIVER_DREAMS_USE_COLOR_VARIANTS"
 
-char *
-choose_symbol_by_environment(char *default_symbol, char *fallback_symbol)
+const char *
+choose_symbol_by_environment(const char *default_symbol, const char *fallback_symbol)
 {
 	return !strcmp(getenv(ENV_FALLBACK_TEXT_KEY), "0")
 	? default_symbol
 	: fallback_symbol;
 }
 
-char *
-choose_color_by_environment(char *default_color, char *color_variant)
+const char *
+choose_color_by_environment(const char *default_color, const char *color_variant)
 {
 	return !strcmp(getenv(ENV_COLOR_VARIANTS_KEY), "0")
 	? default_color
