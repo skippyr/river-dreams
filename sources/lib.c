04ad46b97f276f23cda454228be8e124d757d54a
Sherman Rofeman <skippyr.developer@gmail.com>
2023-04-05 15:50:40 -0300

    refactor: remove unecessary define macro

--- sources/lib.c
+++ sources/lib.c
@@ -1,5 +1,4 @@
 #define ENV_FALLBACK_TEXT_KEY "RIVER_DREAMS_USE_FALLBACK_TEXT"
-#define ENV_COLOR_VARIANTS_KEY "RIVER_DREAMS_USE_COLOR_VARIANTS"
 
 const char *
 choose_symbol_by_environment(const char *default_symbol, const char *fallback_symbol)
