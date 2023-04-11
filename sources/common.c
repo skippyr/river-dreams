dcfd2a428018022d7785f4640cc5fdc70fd9c7b1
Sherman Rofeman <skippyr.developer@gmail.com>
2023-04-11 14:37:44 -0300

    refactor: change wrong indentation style in common.c

--- sources/common.c
+++ sources/common.c
@@ -4,7 +4,10 @@ unsigned short int
 is_to_use_fallback_text(void)
 {
 	return (
-		!strcmp(getenv(FALLBACK_TEXT_ENVIRONMENT_VARIABLE), "1")
+		!strcmp(
+			getenv(FALLBACK_TEXT_ENVIRONMENT_VARIABLE),
+			"1"
+		)
 		? 1
 		: 0
 	);
