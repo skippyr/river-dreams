522c735b0614122959405fd2df2f97a1edacd1fa
Sherman Rofeman <skippyr.developer@gmail.com>
2023-04-20 11:09:15 -0300

    refactor: simplify return of is_to_use_fallback_text()

--- sources/common.c
+++ sources/common.c
@@ -3,17 +3,14 @@
 unsigned short int is_to_use_fallback_text(void)
 {
 	char *fallback_text = getenv(FALLBACK_TEXT_ENVIRONMENT_VARIABLE);
-	if (fallback_text == NULL)
-	{
-		return (0);
-	}
 	return (
-		!strcmp(
+		fallback_text == NULL ||
+		strcmp(
 			fallback_text,
 			"1"
 		) ?
-		1 :
-		0
+		0 :
+		1
 	);
 }
 
