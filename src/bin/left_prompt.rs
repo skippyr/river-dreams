a55b33e0a03b8413958cc10cd7f1f086412394ec
Sherman Rofeman <skippyr.developer@gmail.com>
2023-05-02 19:18:53 -0300

    feat: add new objects to time module and function to create calendar component

--- src/bin/left_prompt.rs
+++ src/bin/left_prompt.rs
@@ -11,7 +11,7 @@ use river_dreams::{
 	},
 	styles::Color,
 	terminal::Terminal,
-	math::is_even,
+	math::is_even, time::Calendar,
 };
 
 fn create_commands_separator_component() -> PromptComponent
@@ -46,10 +46,23 @@ fn create_top_left_connector_component() -> PromptComponent
 	connector.set_default_content(String::from("╭"));
 	connector.set_color(Color::Red);
 	component.append_string_to_structure(connector.as_string());
-	let mut curly_brackets: Text = Text::new();
-	curly_brackets.set_content(String::from("{"));
-	curly_brackets.set_color(Color::Yellow);
-	component.append_string_to_structure(curly_brackets.as_string());
+	let mut left_curly_bracket: Text = Text::new();
+	left_curly_bracket.set_content(String::from("{"));
+	left_curly_bracket.set_color(Color::Yellow);
+	component.append_string_to_structure(left_curly_bracket.as_string());
+	component
+}
+
+fn create_calendar_component() -> PromptComponent
+{
+	let mut component: PromptComponent = PromptComponent::new();
+	let calendar: Calendar = Calendar::from_current_moment();
+	let mut symbol: TextWithFallback = TextWithFallback::new();
+	symbol.set_default_content(String::from(" "));
+	symbol.set_fallback_content(String::from("Calendar "));
+	symbol.set_color(Color::Red);
+	component.append_string_to_structure(symbol.as_string());
+	component.append_string_to_structure(calendar.as_string());
 	component
 }
 
@@ -58,6 +71,7 @@ fn main()
 	let mut left_prompt: Prompt = Prompt::new();
 	left_prompt.add_component(create_commands_separator_component());
 	left_prompt.add_component(create_top_left_connector_component());
+	left_prompt.add_component(create_calendar_component());
 	print!(
 		"{}",
 		left_prompt.as_string()
--- src/time.rs
+++ src/time.rs
@@ -1,59 +1,128 @@
 use chrono::
 {
 	DateTime,
-	Local,
-	Datelike
+	Local, Datelike
 };
 
-pub enum DayMoment
+pub struct Month
+{ value: u32 }
+
+impl Month
 {
-	Dawn,
-	Morning,
-	Afternoon,
-	Night
+	pub fn from(value: u32) -> Month
+	{ Month { value } }
+
+	pub fn as_abbreviated_string(&self) -> Option<String>
+	{
+		match self.value
+		{
+			1 =>
+			{ Some(String::from("Jan")) }
+			2 =>
+			{ Some(String::from("Feb")) }
+			3 =>
+			{ Some(String::from("Mar")) }
+			4 =>
+			{ Some(String::from("Apr")) }
+			5 =>
+			{ Some(String::from("May")) }
+			6 =>
+			{ Some(String::from("Jun")) }
+			7 =>
+			{ Some(String::from("Jul")) }
+			8 =>
+			{ Some(String::from("Aug")) }
+			9 =>
+			{ Some(String::from("Sep")) }
+			10 =>
+			{ Some(String::from("Oct")) }
+			11 =>
+			{ Some(String::from("Nov")) }
+			12 =>
+			{ Some(String::from("Dec")) }
+			_ =>
+			{ None }
+		}
+	}
 }
 
-pub fn get_day_moment(hour_in_24h_format: u32) -> DayMoment
+pub struct WeekDay
+{ value: u32 }
+
+impl WeekDay
 {
-	let is_dawn: bool = hour_in_24h_format < 6;
-	let is_morning: bool = !is_dawn && hour_in_24h_format < 12;
-	let is_afternoon: bool = !is_morning && hour_in_24h_format < 18;
-	if is_dawn
-	{ DayMoment::Dawn }
-	else if is_morning
-	{ DayMoment::Morning }
-	else if is_afternoon
-	{ DayMoment::Afternoon }
-	else
-	{ DayMoment::Night }
+	pub fn from(value: u32) -> WeekDay
+	{ WeekDay { value } }
+
+	pub fn as_abbreviated_string(&self) -> Option<String>
+	{
+		match self.value
+		{
+			0 =>
+			{ Some(String::from("Sun")) }
+			1 =>
+			{ Some(String::from("Mon")) }
+			2 =>
+			{ Some(String::from("Tue")) }
+			3 =>
+			{ Some(String::from("Wed")) }
+			4 =>
+			{ Some(String::from("Thu")) }
+			5 =>
+			{ Some(String::from("Fri")) }
+			6 =>
+			{ Some(String::from("Sat")) }
+			_ =>
+			{ None }
+		}
+	}
 }
 
-pub fn get_day_of_month_ordinal_suffix(day_of_month: i8) -> String
+pub struct Calendar
 {
-	let is_first: bool = (day_of_month - 1) % 10 == 0;
-	let is_second: bool = (day_of_month - 2) % 10 == 0;
-	let is_third: bool = (day_of_month - 3) % 10 == 0;
-	if is_first
-	{ String::from("st") }
-	else if is_second
-	{ String::from("nd") }
-	else if is_third
-	{ String::from("rd") }
-	else
-	{ String::from("th") }
+	day: u32,
+	month: Month,
+	week_day: WeekDay,
+	year: i32
 }
 
-pub fn get_calendar_string(local_time: &DateTime<Local>) -> String
+impl Calendar
 {
-	local_time
-		.format(&format!(
-			"(%a) %b {}{}, %Y",
-			local_time.day(),
-			get_day_of_month_ordinal_suffix(local_time.day() as i8)
-		))
-		.to_string()
-}
+	pub fn from_current_moment() -> Calendar
+	{
+		let local_time: DateTime<Local> = Local::now();
+		Calendar
+		{
+			day: local_time.day(),
+			month: Month::from(local_time.month()),
+			week_day: WeekDay::from(local_time.weekday().num_days_from_sunday()),
+			year: local_time.year()
+		}
+	}
 
-pub fn get_clock_string(local_time: &DateTime<Local>) -> String
-{ local_time.format("%Hh%Mm").to_string() }
+	pub fn as_string(&self) -> String
+	{
+		let month_as_abbreviated_string: String = match self.month.as_abbreviated_string()
+		{
+			Some(month_as_abbreviated_string) =>
+			{ month_as_abbreviated_string }
+			None =>
+			{ String::new() }
+		};
+		let week_day_as_abbreviated_string: String = match self.week_day.as_abbreviated_string()
+		{
+			Some(week_day_as_abbreviated_string) =>
+			{ week_day_as_abbreviated_string }
+			None =>
+			{ String::new() } 
+		};
+		format!(
+			"({}) {} {}, {}",
+			week_day_as_abbreviated_string,
+			month_as_abbreviated_string,
+			self.day,
+			self.year
+		)
+	}
+}
 
