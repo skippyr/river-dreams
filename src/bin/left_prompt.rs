ae33fbc3d53e902701114835ccff5d28da2321b3
Sherman Rofeman <skippyr.developer@gmail.com>
2023-05-02 16:35:48 -0300

    refactor: make terminal module contain an struct

--- src/bin/left_prompt.rs
+++ src/bin/left_prompt.rs
@@ -1,263 +1,34 @@
-use river_dreams::
-{
-	terminal::get_terminal_width,
-	shell::
+use river_dreams::{
+	prompt::
 	{
-		get_exit_code_string,
-		get_user
+		Prompt,
+		PromptComponent
 	},
-	styles::
-	{
-		Color,
-		colorize,
-		get_status_color
-	},
-	symbols::
-	{
-		Symbol,
-		get_commands_separator_symbol,
-		get_clock_symbol
-	},
-	time::
-	{
-		get_calendar_string,
-		get_clock_string
-	},
-	file_system::
-	{
-		get_disk_usage_percentage,
-		get_current_directory_path_abbreviated, does_user_have_ownership_of_current_directory_path
-	}, environment_variables::get_python_environment
-};
-use chrono::
-{
-	DateTime,
-	Local
+	terminal::Terminal,
+	math::is_even
 };
 
-fn print_top_left_decorator()
+fn create_commands_separator_component() -> PromptComponent
 {
-	let decorator: Symbol = Symbol
+	let mut body: String = String::new();
+	let terminal: Terminal = Terminal::new();
+	for column in 0..terminal.get_columns()
 	{
-		default_text: String::from("╭─"),
-		fallback_text: String::from("┌─"),
-		color: Color::Red
-	};
-	print!(
-		"{}{}",
-		colorize(
-			decorator.get_text_for_environment(),
-			decorator.color
-		),
-		colorize(
-			String::from("{"),
-			Color::Yellow
-		)
-	);
-}
-
-fn print_top_right_decorator()
-{
-	println!(
-		"{}",
-		colorize(
-			String::from("}"),
-			Color::Yellow
-		)
-	);
-}
-
-fn print_bottom_left_decorator()
-{
-	let decorator: Symbol = Symbol
-	{
-		default_text: String::from("╰"),
-		fallback_text: String::from("└"),
-		color: Color::Red
-	};
-	print!(
-		"{}",
-		colorize(
-			decorator.get_text_for_environment(),
-			decorator.color
-		)
-	);
-}
-
-fn print_commands_separator()
-{
-	for column in 0..get_terminal_width()
-	{
-		let symbol: Symbol = get_commands_separator_symbol(column);
-		print!(
-			"{}",
-			colorize(
-				symbol.get_text_for_environment(),
-				symbol.color
-			)
-		);
+		if is_even(column)
+		{ body.push_str("a") }
+		else
+		{ body.push_str("b") }
 	}
-}
-
-fn print_horizontal_separator()
-{
-	print!(
-		" {} ",
-		colorize(
-			String::from("¦"),
-			Color::Red
-		)
-	);
-}
-
-fn print_disk_usage_percentage()
-{
-	let symbol: Symbol = Symbol
-	{
-		default_text: String::from(" "),
-		fallback_text: String::from("Disk "),
-		color: Color::Yellow
-	};
-	print!(
-		"{}{}%%",
-		colorize(
-			symbol.get_text_for_environment(),
-			symbol.color
-		),
-		get_disk_usage_percentage()
-	);
-}
-
-fn print_calendar(local_time: &DateTime<Local>)
-{
-	let symbol: Symbol = Symbol
-	{
-		default_text: String::from(" "),
-		fallback_text: String::from("Calendar "),
-		color: Color::Red
-	};
-	print!(
-		"{}{}",
-		colorize(
-			symbol.get_text_for_environment(),
-			symbol.color
-		),
-		get_calendar_string(local_time)
-	);
-}
-
-fn print_clock(local_time: &DateTime<Local>)
-{
-	let symbol: Symbol = get_clock_symbol(local_time);
-	print!(
-		"{}{}",
-		colorize(
-			symbol.get_text_for_environment(),
-			symbol.color
-		),
-		get_clock_string(local_time)
-	);
-}
-
-fn print_shell_status()
-{
-	let error_symbol: Symbol = Symbol
-	{
-		default_text: String::from(" "),
-		fallback_text: String::from("X "),
-		color: Color::Red
-	};
-	let arrow_symbol: Symbol = Symbol
-	{
-		default_text: String::from("⤐  "),
-		fallback_text: String::from("> "),
-		color: get_status_color()
-	};
-	print!(
-		"{}{}",
-		get_exit_code_string(&error_symbol),
-		colorize(
-			arrow_symbol.get_text_for_environment(),
-			arrow_symbol.color
-		)
-	);
-}
-
-fn print_user()
-{
-	print!(
-		"{} ",
-		colorize(
-			get_user(),
-			Color::Cyan
-		)
-	);
-}
-
-fn print_python_environment()
-{
-	let python_environment: String = match get_python_environment()
-	{
-		Some(python_environment) =>
-		{ python_environment }
-		None =>
-		{ return }
-	};
-	print!(
-		"using {} ",
-		colorize(
-			python_environment,
-			Color::Magenta
-		)
-	);
-}
-
-fn print_current_directory_path_abbreviated()
-{
-	print!(
-		"at {}",
-		colorize(
-			get_current_directory_path_abbreviated(),
-			Color::Green
-		)
-	);
-}
-
-fn print_directory_ownership()
-{
-	if does_user_have_ownership_of_current_directory_path()
-	{ return; }
-	let symbol: Symbol = Symbol
-	{
-		default_text: String::from(""),
-		fallback_text: String::from("#"),
-		color: Color::Red
-	};
-	print!(
-		" {}",
-		colorize(
-			symbol.get_text_for_environment(),
-			symbol.color
-		)
-	)
+	PromptComponent::new(body)
 }
 
 fn main()
 {
-	let local_time: DateTime<Local> = Local::now();
-	print_commands_separator();
-	print_top_left_decorator();
-	print_disk_usage_percentage();
-	print_horizontal_separator();
-	print_calendar(&local_time);
-	print_horizontal_separator();
-	print_clock(&local_time);
-	print_top_right_decorator();
-	print_bottom_left_decorator();
-	print_shell_status();
-	print_user();
-	print_python_environment();
-	print_current_directory_path_abbreviated();
-	print_directory_ownership();
+	let mut left_prompt: Prompt = Prompt::new();
+	left_prompt.add_component(create_commands_separator_component());
+	print!(
+		"{}",
+		left_prompt.as_string()
+	);
 }
 
--- src/terminal.rs
+++ src/terminal.rs
@@ -2,21 +2,40 @@ use super::error_treatment::print_error_message;
 use std::process::exit;
 use crossterm::terminal::size;
 
-pub fn get_terminal_width() -> u16
+pub struct Terminal
 {
-	match size()
+	columns: u16,
+	lines: u16
+}
+
+impl Terminal
+{
+	pub fn new() -> Terminal
 	{
-		Ok(terminal_size) =>
+		let (
+			columns,
+			lines
+		) = match size()
 		{
-			let terminal_width: u16 = terminal_size.0;
-			let _terminal_height: u16 = terminal_size.1;
-			terminal_width
-		}
-		Err(_error) =>
+			Ok(size) =>
+			{ size }
+			Err(_error) =>
+			{
+				print_error_message("Could not get output stream width.");
+				exit(1);
+			}
+		};
+		Terminal
 		{
-			print_error_message("Could not get output stream width.");
-			exit(1);
+			columns,
+			lines
 		}
 	}
+	
+	pub fn get_columns(&self) -> u16
+	{ self.columns }
+
+	pub fn get_lines(&self) -> u16
+	{ self.lines }
 }
 
