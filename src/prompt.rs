3be9c07520bac9bd7487db3fa9a51f32bfc96e3f
Sherman Rofeman <skippyr.developer@gmail.com>
2023-05-09 11:36:26 -0300

    refactor: add Prompt, PromptComponent and PromptString

--- src/prompt.rs
+++ src/prompt.rs
@@ -62,3 +62,51 @@ impl Display for PromptString
 	}
 }
 
+pub struct PromptComponent
+{ structure: String }
+
+impl PromptComponent
+{
+	pub fn new() -> PromptComponent
+	{ PromptComponent { structure: String::new() } }
+
+	pub fn append_to_structure(
+		&mut self,
+		structure_component: String
+	)
+	{ self.structure.push_str(&structure_component) }
+
+	pub fn get_structure(&self) -> String
+	{ self.structure.clone() }
+}
+
+pub struct Prompt
+{ structure: String }
+
+impl Prompt
+{
+	pub fn new() -> Prompt
+	{ Prompt { structure: String::new() } }
+
+	pub fn append_to_structure(
+		&mut self,
+		structure_component: String
+	)
+	{ self.structure.push_str(&structure_component) }
+}
+
+impl Display for Prompt
+{
+	fn fmt(
+		&self,
+		formatter: &mut Formatter
+	) -> Result
+	{
+		write!(
+			formatter,
+			"{}",
+			self.structure
+		)
+	}
+}
+
--- src/prompt/mod.rs
+++
@@ -1,2 +0,0 @@
-pub mod prompt_string;
-
