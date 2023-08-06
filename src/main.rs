ce9f95143646d8950312ae562e3821959af25e5c
Sherman Rofeman <skippyr.developer@gmail.com>
2023-08-06 12:20:28 -0300

    ~% finish refactor to remaining functions

--- src/main.rs
+++ src/main.rs
@@ -16,9 +16,9 @@ fn Print_Spacing()
 
 fn Print_Commands_Separator()
 {
-    let size = crossterm::terminal::size().unwrap_or_else(
-        |__error| {Throw_Error(
-            "can not get terminal size.");});
+    let size = crossterm::terminal::size().unwrap_or_else(|__error| {
+        Throw_Error("can not get terminal size.");
+    });
     for column in 0..size.0 {
         print!("{}%f", if column % 2 == 0 {"%F{3}⊵"} else {"%F{1}⊼"});
     }
@@ -39,7 +39,7 @@ fn Get_Main_Disk(partitions: &[sysinfo::Disk]) -> String
         if partition.mount_point() == std::path::PathBuf::from("/") {
             if let Some(name) = partition.name().to_str() {
                 let mut characters = name.chars()
-                                                    .collect::<Vec<char>>();
+                                         .collect::<Vec<char>>();
                 characters.pop();
                 return characters.iter().collect();
             }
@@ -66,8 +66,7 @@ fn Get_Disk_Usage() -> i32
     }
     let mut usage_percentage = 0;
     if total_size_in_bytes != 0 {
-        let used_size_in_bytes = total_size_in_bytes -
-                                      available_size_in_bytes;
+        let used_size_in_bytes = total_size_in_bytes - available_size_in_bytes;
         usage_percentage = ((used_size_in_bytes as f32 / total_size_in_bytes as
                              f32) * 100.0) as i32;
     }
@@ -79,11 +78,105 @@ fn Print_Disk_Usage()
     print!("%F{{3}}󰋊 %f{}%%", Get_Disk_Usage());
 }
 
+fn Get_Month(moment: &chrono::DateTime<chrono::Local>) -> String
+{
+    match moment.month0() {
+        1 => {"Feb"}
+        2 => {"Mar"}
+        3 => {"Apr"}
+        4 => {"May"}
+        5 => {"Jun"}
+        6 => {"Jul"}
+        7 => {"Aug"}
+        8 => {"Sep"}
+        9 => {"Oct"}
+        10 => {"Nov"}
+        _ => "Jan"
+    }.to_string()
+}
+
+fn Get_Ordinal(value: u32) -> String
+{
+    if value - 1 % 10 == 0 {
+        "st"
+    }
+    else if value - 2 % 10 == 0 {
+        "nd"
+    }
+    else if value - 3 % 10 == 0 {
+        "rd"
+    }
+    else {
+        "th"
+    }.to_string()
+}
+
+fn Print_Calendar(moment: &chrono::DateTime<chrono::Local>)
+{
+    let day = moment.day();
+    print!("%F{{1}}󰸗 %f({}) {} {day}{}", moment.weekday(), Get_Month(moment),
+           Get_Ordinal(day));
+}
+
+fn Get_Clock_Symbol(hour: u32) -> String
+{
+    if hour < 6 {
+        "%F{6}󰭎"
+    }
+    else if hour < 12 {
+        "%F{1}󰖨  "
+    }
+    else if hour < 18 {
+        "%F{4} "
+    }
+    else {
+        "%F{3}󰽥 "
+    }.to_string()
+}
+
+fn Format_Time(time: u32) -> String
+{
+    format!("{}{time}", if time < 10 {"0"} else {""})
+}
+
+fn Print_Clock(moment: &chrono::DateTime<chrono::Local>)
+{
+    let hour = moment.hour();
+    print!("{}%f{}h{}m", Get_Clock_Symbol(hour), Format_Time(hour),
+           Format_Time(moment.minute()));
+}
+
+fn Print_Virtual_Environment()
+{
+    if let Ok(virtual_environment) = std::env::var("VIRTUAL_ENV") {
+        let description = "can not get virtual environment";
+        let virtual_environment = std::path::PathBuf::from(virtual_environment);
+        let virtual_environment = virtual_environment
+            .file_name()
+            .unwrap_or_else(|| {
+                Throw_Error(description)
+            })
+            .to_str()
+            .unwrap_or_else(|| {
+                Throw_Error(description);
+            });
+        print!("%F{{6}}(%f{virtual_environment}%F{{6}}) ");
+    }
+}
+
 fn main()
 {
+    let moment = chrono::Local::now();
     Print_Commands_Separator();
     print!("%F{{3}}:«");
     Print_Local_Ip_Address();
     Print_Spacing();
     Print_Disk_Usage();
+    Print_Spacing();
+    Print_Calendar(&moment);
+    Print_Spacing();
+    Print_Clock(&moment);
+    print!("%F{{3}}»:\n%(#.{{%F{{1}}#%F{{3}}}}.){{%(?.≗.%F{{1}}⨲%F{{3}})}}⤐  ");
+    Print_Virtual_Environment();
+    print!("%F{{1}}%1~ %F{{6}}⩺ %f");
 }
