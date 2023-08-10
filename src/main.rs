98c797ef45fb096a93e3ce38f8f426a36abe04f2
Sherman Rofeman <skippyr.developer@gmail.com>
2023-08-10 10:32:19 -0300

    ~% make returns explicit

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 #![allow(nonstandard_style)]
+#![allow(unused_parens)]
 
 use chrono::{Datelike, Timelike};
 use sysinfo::{DiskExt, SystemExt};
@@ -46,7 +47,7 @@ fn Get_Main_Disk(partitions: &[sysinfo::Disk]) -> String
                 let mut characters = name.chars()
                                          .collect::<Vec<char>>();
                 characters.pop();
-                return characters.iter().collect();
+                return (characters.iter().collect());
             }
         }
     }
@@ -79,7 +80,7 @@ fn Get_Disk_Usage() -> i32
         usage_percentage = ((used_size_in_bytes as f32 / total_size_in_bytes as
                              f32) * 100.0) as i32;
     }
-    usage_percentage
+    return (usage_percentage);
 }
 
 fn Print_Disk_Usage()
@@ -89,40 +90,44 @@ fn Print_Disk_Usage()
 
 fn Get_Month(moment: &chrono::DateTime<chrono::Local>) -> String
 {
-    match moment.month0()
-    {
-        1 => {"Feb"}
-        2 => {"Mar"}
-        3 => {"Apr"}
-        4 => {"May"}
-        5 => {"Jun"}
-        6 => {"Jul"}
-        7 => {"Aug"}
-        8 => {"Sep"}
-        9 => {"Oct"}
-        10 => {"Nov"}
-        _ => {"Jan"}
-    }.to_string()
+    return (
+        match moment.month0()
+        {
+            1 => {"Feb"}
+            2 => {"Mar"}
+            3 => {"Apr"}
+            4 => {"May"}
+            5 => {"Jun"}
+            6 => {"Jul"}
+            7 => {"Aug"}
+            8 => {"Sep"}
+            9 => {"Oct"}
+            10 => {"Nov"}
+            _ => {"Jan"}
+        }.to_string()
+    );
 }
 
 fn Get_Ordinal(day: u32) -> String
 {
-    if day - 1 % 10 == 0
-    {
-        "st"
-    }
-    else if day - 2 % 10 == 0
-    {
-        "nd"
-    }
-    else if day - 3 % 10 == 0
-    {
-        "rd"
-    }
-    else
-    {
-        "th"
-    }.to_string()
+    return (
+        if day - 1 % 10 == 0
+        {
+            "st"
+        }
+        else if day - 2 % 10 == 0
+        {
+            "nd"
+        }
+        else if day - 3 % 10 == 0
+        {
+            "rd"
+        }
+        else
+        {
+            "th"
+        }.to_string()
+    );
 }
 
 fn Print_Calendar(moment: &chrono::DateTime<chrono::Local>)
@@ -134,27 +139,29 @@ fn Print_Calendar(moment: &chrono::DateTime<chrono::Local>)
 
 fn Get_Clock_Symbol(hour: u32) -> String
 {
-    if hour < 6
-    {
-        "%F{6}󰭎"
-    }
-    else if hour < 12
-    {
-        "%F{1}󰖨  "
-    }
-    else if hour < 18
-    {
-        "%F{4} "
-    }
-    else
-    {
-        "%F{3}󰽥 "
-    }.to_string()
+    return (
+        if hour < 6
+        {
+            "%F{6}󰭎"
+        }
+        else if hour < 12
+        {
+            "%F{1}󰖨  "
+        }
+        else if hour < 18
+        {
+            "%F{4} "
+        }
+        else
+        {
+            "%F{3}󰽥 "
+        }.to_string()
+    );
 }
 
 fn Format_Time(time: u32) -> String
 {
-    format!("{}{time}", if time < 10 {"0"} else {""})
+    return(format!("{}{time}", if time < 10 {"0"} else {""}));
 }
 
 fn Print_Clock(moment: &chrono::DateTime<chrono::Local>)
