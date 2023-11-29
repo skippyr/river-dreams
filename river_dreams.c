d523a5abd432798aa89dab42272751cee099d629
Sherman Rofeman <skippyr.developer@gmail.com>
2023-11-29 09:57:57 -0300

    add calendar

--- river_dreams.c
+++ river_dreams.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <sys/ioctl.h>
+#include <time.h>
 
 static void
 write_cmd_sep(void)
@@ -11,10 +12,25 @@ write_cmd_sep(void)
 		printf(s.ws_col % 2 ? "%%F{1}⊼" : "%%F{3}⊵");
 }
 
+static void
+write_cal(struct tm *t)
+{
+	char *wdays[] = {"Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"},
+	     *mons[] = {"Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep",
+			"Oct", "Nov", "Dec"};
+	printf("%%F{1}󰸗 %%f(%s) %s %d%s", wdays[t->tm_wday], mons[t->tm_mon],
+	       t->tm_mday, !((t->tm_mday - 1) % 10) ? "st" : !((t->tm_mday - 2) % 10) ?
+	       "nd" : !((t->tm_mday - 3) % 10) ? "rd" : "th");
+}
+
 int
 main(void)
 {
+	time_t e = time(NULL);
+	struct tm t;
+	localtime_r(&e, &t);
 	write_cmd_sep();
+	write_cal(&t);
 	printf("%%F{3}⤐  %%F{1}%%1~%%f ");
 	return 0;
 }
