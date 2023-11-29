290547f916b893a3bf71a34bd394fa35ed8595fc
Sherman Rofeman <skippyr.developer@gmail.com>
2023-11-29 09:33:47 -0300

    add cmd sep

--- river_dreams.c
+++ river_dreams.c
@@ -2,8 +2,19 @@
 #include <stdlib.h>
 #include <sys/ioctl.h>
 
+static void
+write_cmd_sep(void)
+{
+	struct winsize s;
+	ioctl(2, TIOCGWINSZ, &s);
+	for (; s.ws_col; s.ws_col--)
+		printf(s.ws_col % 2 ? "%%F{1}⊼" : "%%F{3}⊵");
+}
+
 int
 main(void)
 {
+	write_cmd_sep();
+	printf("%%F{3}⤐  %%F{1}%%1~%%f ");
 	return 0;
 }
