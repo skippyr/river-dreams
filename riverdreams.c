13899f2346883bec6e09dbb7a00a961ee07d888b
Sherman Rofeman <skippyr.developer@gmail.com>
2023-12-02 11:45:35 -0300

    prepare release v6.1.0
    - change date format.
    - add battery support.

--- riverdreams.c
+++ riverdreams.c
@@ -1,47 +1,62 @@
 /* See LICENSE file for copyright and license details. */
+#include <arpa/inet.h>
+#include <fcntl.h>
 #include <ifaddrs.h>
 #include <net/if.h>
-#include <arpa/inet.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <sys/ioctl.h>
 #include <sys/statvfs.h>
 #include <time.h>
+#include <unistd.h>
 
-#define ISORD(n) !((t.tm_mday - n) % 10)
-#define ISDWN t.tm_hour < 6
-#define ISMRN t.tm_hour < 12
-#define ISAFT t.tm_hour < 18
+#define LN(a, b) for (int i = 0; i < s.ws_col; i++) printf(i % 2 ? a : b);
+#ifndef PS
+#define PS "/sys/class/power_supply"
+#endif
 
-static void writevsep(void);
-static void writehsep(void);
-static void writeip(void);
-static void writedisk(void);
-static void writetm(void);
+void bat(void);
+void disk(void);
+void ip(void);
+void tm(void);
 
-static void
-writevsep(void)
+void
+bat(void)
 {
-	struct winsize s;
-	ioctl(2, TIOCGWINSZ, &s);
-	for (; s.ws_col; s.ws_col--)
-		printf(s.ws_col % 2 ? "%%F{1}⊼" : "%%F{3}⊵");
+	char z[1], w[5] = "0";
+	int s = open(PS "/BAT0/status", O_RDONLY),
+	    c = open(PS "/BAT0/capacity", O_RDONLY);
+	if (s < 0)
+		return;
+	if (c > 0)
+		read(c, w, sizeof(w));
+	read(s, z, 1);
+	close(s);
+	close(c);
+	int p = atoi(w);
+	printf("%s%s%%f%d%%%%  ", *z == 'C' ? "%F{3}󱐋 " : "", p <= 5 ?
+	       "%F{1}  " : p <= 25 ? "%F{3}  " : p <= 50 ? "%F{2}  " :
+	       "%F{2}  ", p);
 }
 
-static void
-writehsep(void)
+void
+disk(void)
 {
-	printf("  ");
+	struct statvfs s;
+	statvfs("/", &s);
+	long unsigned t = s.f_frsize * s.f_blocks, r = s.f_frsize * s.f_bavail;
+	int u = ((float)(t - r) / t) * 100;
+	printf("%%F{%d}󰋊 %%f%d%%%%  ", u < 70 ? 2 : u < 80 ? 3 : 1, u);
 }
 
-static void
-writeip(void)
+void
+ip(void)
 {
-	struct ifaddrs *a;
+	struct ifaddrs *a, *t;
 	char z[16] = "127.0.0.1";
-	printf("%%F{4} %%f");
 	getifaddrs(&a);
-	for (struct ifaddrs *t = a; t; t = t->ifa_next)
+	printf("%%F{4} %%f");
+	for (t = a; t; t = t->ifa_next)
 		if (t->ifa_addr && t->ifa_addr->sa_family & AF_INET &&
 		    t->ifa_flags & IFF_RUNNING &&
 		    !(t->ifa_flags & IFF_LOOPBACK)) {
@@ -50,46 +65,35 @@ writeip(void)
 				  z, sizeof(z));
 			break;
 		}
-	printf("%s", z);
+	printf("%s  ", z);
 	freeifaddrs(a);
 }
 
-static void
-writedisk(void)
-{
-	struct statvfs s;
-	statvfs("/", &s);
-	unsigned long t = s.f_frsize * s.f_blocks, r = s.f_frsize * s.f_bavail;
-	int u = ((float)(t - r) / t) * 100;
-	printf("%%F{%d}󰋊 %%f%d%%%%", u < 70 ? 2 : u < 80 ? 3 : 1, u);
-}
-
-static void
-writetm(void)
+void
+tm(void)
 {
 	time_t e = time(0);
 	struct tm t;
-	char *w[] = {"Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"},
-	     *m[] = {"Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug",
-		     "Sep", "Oct", "Nov", "Dec"};
+	char b[19];
 	localtime_r(&e, &t);
-	printf("%%F{%d}%s%%f(%s) %s %d%s %02dh%02dm", ISDWN ? 5 : ISMRN ? 1 :
-	       ISAFT ? 4 : 3, ISDWN ? "󰭎 " : ISMRN ? "󰖨 " : ISAFT ? " " : "󰽥 ",
-	       w[t.tm_wday], m[t.tm_mon], t.tm_mday, ISORD(1) ? "st" :
-	       ISORD(2) ? "nd" : ISORD(3) ? "rd" : "st", t.tm_hour, t.tm_min);
+	strftime(b, sizeof(b), "(%a) %b %d %H:%M", &t);
+	printf("%s%%f%s", t.tm_hour < 6 ? "%F{6}󰭎 " : t.tm_hour < 12 ?
+	       "%F{1}󰖨 " : t.tm_hour < 18 ? "%F{4} " : "%F{3}󰽥 ", b);
 }
 
 int
 main(void)
 {
-	writevsep();
-	printf("%%F{3}:«(");
-	writeip();
-	writehsep();
-	writedisk();
-	writehsep();
-	writetm();
+	struct winsize s;
+	ioctl(2, TIOCGWINSZ, &s);
+	LN("%%F{1}⊼", "%%F{3}⊵");
+	LN("%%F{1}-", "%%F{3}=");
+	printf("\n\33[1A%%F{3}:«(");
+	ip();
+	disk();
+	bat();
+	tm();
 	printf("%%F{3})»:\n%%(#.{%%F{1}#%%F{3}}.){%%(?.≗.%%F{1}⨲)%%F{3}}⤐  "
-	       "%%F{1}%%1~%%f ");
+	       "%%F{1}%%~%%f ");
 	return 0;
 }
