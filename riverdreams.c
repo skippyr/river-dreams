7a6af5e6d70d3ecb0b9a3a61be5d621541cbbc1b
Sherman Rofeman <skippyr.developer@gmail.com>
2023-12-04 21:25:25 +0000

    prepare v6.1.1
    - separate calendar and clock.
    - add cursor decorator.

--- riverdreams.c
+++ riverdreams.c
@@ -11,14 +11,16 @@
 #include <unistd.h>
 
 #define LN(a, b) for (int i = 0; i < s.ws_col; i++) printf(i % 2 ? a : b);
+#define ORD(n) !((t->tm_mday - n) % 10)
 #ifndef PS
 #define PS "/sys/class/power_supply"
 #endif
 
 void bat(void);
+void cal(struct tm *t);
+void clk(struct tm *t);
 void disk(void);
 void ip(void);
-void tm(void);
 
 void
 bat(void)
@@ -37,6 +39,23 @@ bat(void)
 	printf("%s%s%%f%d%%%%  ", *z == 'C' ? "%F{3}󱐋 " : "", p <= 5 ?
 	       "%F{1}  " : p <= 25 ? "%F{3}  " : p <= 50 ? "%F{2}  " :
 	       "%F{2}  ", p);
+}
+
+void
+cal(struct tm *t)
+{
+	char b[10];
+	strftime(b, sizeof(b), "(%a) %b", t);
+	printf("%%F{1}󰃭 %%f%s %d%s  ", b, t->tm_mday, ORD(1) ? "st" : ORD(2) ?
+	       "nd" : ORD(3) ? "rd" : "th");
+}
+
+void
+clk(struct tm *t)
+{
+	printf("%s%%f%02dh%02dm", t->tm_hour < 6 ? "%F{6}󰭎 " : t->tm_hour < 12 ?
+	       "%F{1}󰖨 " : t->tm_hour < 18 ? "%F{4} " : "%F{3}󰽥 ", t->tm_hour,
+	       t->tm_min);
 }
 
 void
@@ -69,31 +88,23 @@ ip(void)
 	freeifaddrs(a);
 }
 
-void
-tm(void)
-{
-	time_t e = time(0);
-	struct tm t;
-	char b[19];
-	localtime_r(&e, &t);
-	strftime(b, sizeof(b), "(%a) %b %d %H:%M", &t);
-	printf("%s%%f%s", t.tm_hour < 6 ? "%F{6}󰭎 " : t.tm_hour < 12 ?
-	       "%F{1}󰖨 " : t.tm_hour < 18 ? "%F{4} " : "%F{3}󰽥 ", b);
-}
-
 int
 main(void)
 {
+	time_t e = time(0);
+	struct tm t;
 	struct winsize s;
 	ioctl(2, TIOCGWINSZ, &s);
+	localtime_r(&e, &t);
 	LN("%%F{1}⊼", "%%F{3}⊵");
 	LN("%%F{1}-", "%%F{3}=");
 	printf("\n\33[1A%%F{3}:«(");
 	ip();
 	disk();
 	bat();
-	tm();
+	cal(&t);
+	clk(&t);
 	printf("%%F{3})»:\n%%(#.{%%F{1}#%%F{3}}.){%%(?.≗.%%F{1}⨲)%%F{3}}⤐  "
-	       "%%F{1}%%~%%f ");
+	       "%%F{1}%%~ %%F{6}✗%%f  ");
 	return 0;
 }
