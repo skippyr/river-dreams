8afad4d50b0b6e579cb668c50d46cacac6b79aed
Sherman Rofeman <skippyr.developer@gmail.com>
2024-02-07 12:35:20 -0300

    replace syscall by getdents64

--- src/rprompt.c
+++ src/rprompt.c
@@ -2,7 +2,6 @@
 #include <dirent.h>
 #include <fcntl.h>
 #include <stdio.h>
-#include <string.h>
 #include <sys/syscall.h>
 #include <unistd.h>
 
@@ -21,10 +20,11 @@ struct dirinfo {
   int lnk;
 };
 
-struct linux_dirent {
-  unsigned long d_ino;
-  unsigned long d_off;
+struct linux_dirent64 {
+  ino64_t d_ino;
+  off64_t d_off;
   unsigned short d_reclen;
+  unsigned char  d_type;
   char d_name[];
 };
 
@@ -33,18 +33,17 @@ static void getdirinfo(struct dirinfo *di) {
   int fd = open(".", O_RDONLY);
   int ents;
   int i;
-  struct linux_dirent *e;
-  memset(di, 0, sizeof(struct dirinfo));
+  struct linux_dirent64 *e;
   if (fd < 0) {
     return;
   }
-  while ((ents = syscall(SYS_getdents, fd, buf, sizeof(buf))) > 0) {
+  while ((ents = syscall(SYS_getdents64, fd, buf, sizeof(buf))) > 0) {
     for (i = 0; i < ents; i += e->d_reclen) {
-      e = (struct linux_dirent *)(buf + i);
-      if (!strcmp(e->d_name, ".") || !strcmp(e->d_name, "..")) {
+      e = (struct linux_dirent64 *)(buf + i);
+      if (e->d_name[0] == '.' && (!e->d_name[1] || (e->d_name[1] == '.' && !e->d_name[2]))) {
         continue;
       }
-      switch (*(buf + i + e->d_reclen - 1)) {
+      switch (e->d_type) {
       case DT_REG: di->reg++; break;
       case DT_DIR: di->dir++; break;
       case DT_BLK: di->blk++; break;
@@ -59,7 +58,7 @@ static void getdirinfo(struct dirinfo *di) {
 }
 
 int main(void) {
-  struct dirinfo di;
+  struct dirinfo di = {0, 0, 0, 0, 0, 0, 0};
   getdirinfo(&di);
   DIRINFO("%F{4} %f", di.reg);
   DIRINFO("%F{3} %f", di.dir);
