bf7f50870a9b451799b2cf2d56df92403eb92c21
Sherman Rofeman <skippyr.developer@gmail.com>
2024-03-06 18:30:32 -0300

    refactor code style

--- src/left-prompt.c
+++ src/left-prompt.c
@@ -16,263 +16,280 @@
 #endif
 #define IS_ORDINAL_DAY(a_ordinal) !((date->tm_mday - a_ordinal) % 10)
 
-static int CountDigits(int number);
-static size_t FindLastSlash(const char* path, size_t length);
-static void FindGitRoot(const char* pwd, char** root, size_t* length);
-static void WriteBatteryCharge(void);
-static void WriteCalendar(struct tm* date);
-static void WriteClock(struct tm* date);
-static void WriteCommandsSeparator(struct winsize* windowSize);
-static void WriteDirectoryAccess(void);
-static void WriteDiskUsage(void);
-static void WriteExitCode(void);
-static void WriteGitBranch(const char* root, size_t length);
-static void WriteLocalIPV4Address(void);
-static void WriteModulesSeparator(struct winsize* windowSize);
-static void WritePath(const char* pwd, const char* gitRoot, size_t gitRootLength);
-static void WriteRootUserStatus(void);
-static void WriteVirtualEnvironment(void);
+static int countDigits(int number);
+static size_t findLastSlash(const char *path, size_t length);
+static void findGitRoot(const char *pwd, char **root, size_t *length);
+static void writeBatteryCharge(void);
+static void writeCalendar(struct tm *date);
+static void writeClock(struct tm *date);
+static void writeCommandsSeparator(struct winsize *windowSize);
+static void writeDirectoryAccess(void);
+static void writeDiskUsage(void);
+static void writeExitCode(void);
+static void writeGitBranch(const char *root, size_t length);
+static void writeLocalIPV4Address(void);
+static void writeModulesSeparator(struct winsize *windowSize);
+static void writePath(const char *pwd, const char *gitRoot, size_t gitRootLength);
+static void writeRootUserStatus(void);
+static void writeVirtualEnvironment(void);
 
 static int g_modulesLength = 41;
 
-static int CountDigits(int number)
+static int countDigits(int number)
 {
-    int digits;
-    for (digits = !number; number; number /= 10)
-    {
-        ++digits;
-    }
-    return digits;
+	int digits;
+	for (digits = !number; number; number /= 10)
+	{
+		++digits;
+	}
+	return (digits);
 }
 
-static size_t FindLastSlash(const char* path, size_t length)
+static size_t findLastSlash(const char *path, size_t length)
 {
-    for (size_t index = length - 1; index; --index)
-    {
-        if (path[index] == '/')
-        {
-            return index;
-        }
-    }
-    return 0;
+	size_t index;
+	for (index = length - 1; index; --index)
+	{
+		if (path[index] == '/')
+		{
+			return (index);
+		}
+	}
+	return (0);
 }
 
-static void FindGitRoot(const char* pwd, char** root, size_t* length)
+static void findGitRoot(const char *pwd, char **root, size_t *length)
 {
-    size_t pwdLength = strlen(pwd);
-    *root = malloc(pwdLength + 6);
-    for (*length = 0; *length < pwdLength; ++*length)
-    {
-        *(*root + *length) = pwd[*length];
-    }
-    while (1)
-    {
-        *(*root + *length) = '/';
-        *(*root + ++*length) = '.';
-        *(*root + ++*length) = 'g';
-        *(*root + ++*length) = 'i';
-        *(*root + ++*length) = 't';
-        *(*root + ++*length) = 0;
-        if (!access(*root, F_OK))
-        {
-            *(*root + (*length -= 5)) = 0;
-            return;
-        }
-        *(*root + (*length -= 5)) = 0;
-        if (!*(*root + 1))
-        {
-            break;
-        }
-        *(*root + (!(*length = FindLastSlash(*root, *length)) ? ++*length : *length)) = 0;
-    }
-    free(*root);
-    *root = NULL;
+	size_t pwdLength = strlen(pwd);
+	*root = malloc(pwdLength + 6);
+	for (*length = 0; *length < pwdLength; ++*length)
+	{
+		*(*root + *length) = pwd[*length];
+	}
+	while (1)
+	{
+		*(*root + *length) = '/';
+		*(*root + ++*length) = '.';
+		*(*root + ++*length) = 'g';
+		*(*root + ++*length) = 'i';
+		*(*root + ++*length) = 't';
+		*(*root + ++*length) = 0;
+		if (!access(*root, F_OK))
+		{
+			*(*root + (*length -= 5)) = 0;
+			return;
+		}
+		*(*root + (*length -= 5)) = 0;
+		if (!*(*root + 1))
+		{
+			break;
+		}
+		*(*root + (!(*length = findLastSlash(*root, *length)) ? ++*length : *length)) = 0;
+	}
+	free(*root);
+	*root = NULL;
 }
 
-static void WriteBatteryCharge(void)
+static void writeBatteryCharge(void)
 {
-    int statusFile = open(BATTERY "/status", O_RDONLY);
-    if (statusFile < 0)
-    {
-        return;
-    }
-    int capacityFile = open(BATTERY "/capacity", O_RDONLY);
-    char capacityBuffer[5];
-    int charge = 0;
-    if (capacityFile > 0)
-    {
-        read(capacityFile, capacityBuffer, sizeof(capacityBuffer));
-        close(capacityFile);
-        charge = atoi(capacityBuffer);
-    }
-    char statusBuffer;
-    read(statusFile, &statusBuffer, sizeof(statusBuffer));
-    close(statusFile);
-    printf("%s%s%%f%d%%%%  ", statusBuffer == 'C' ? "%F{yellow}󱐋 " : "",
-           charge <= 5    ? "%F{red}  "
-           : charge <= 25 ? "%F{yellow}  "
-           : charge <= 50 ? "%F{green}  "
-                          : "%F{green}  ",
-           charge);
-    g_modulesLength += CountDigits(charge) + 6 + (statusBuffer == 'C') * 2;
+	int statusFile = open(BATTERY "/status", O_RDONLY);
+	int capacityFile = open(BATTERY "/capacity", O_RDONLY);
+	char statusBuffer;
+	char capacityBuffer[5];
+	int charge = 0;
+	if (statusFile < 0)
+	{
+		return;
+	}
+	if (capacityFile > 0)
+	{
+		read(capacityFile, capacityBuffer, sizeof(capacityBuffer));
+		close(capacityFile);
+		charge = atoi(capacityBuffer);
+	}
+	read(statusFile, &statusBuffer, sizeof(statusBuffer));
+	close(statusFile);
+	printf("%s%s%%f%d%%%%  ", statusBuffer == 'C' ? "%F{yellow}󱐋 " : "",
+		   charge <= 5    ? "%F{red}  "
+		   : charge <= 25 ? "%F{yellow}  "
+		   : charge <= 50 ? "%F{green}  "
+						  : "%F{green}  ",
+		   charge);
+	g_modulesLength += countDigits(charge) + 6 + (statusBuffer == 'C') * 2;
 }
 
-static void WriteGitBranch(const char* root, size_t length)
+static void writeGitBranch(const char *root, size_t length)
 {
-    if (!root)
-    {
-        return;
-    }
-    char* path = malloc(length + 11);
-    sprintf(path, "%s/.git/HEAD", root);
-    FILE* head = fopen(path, "r");
-    free(path);
-    if (!head)
-    {
-        return;
-    }
-    printf("%%F{3}:«(%%f");
-    for (int character, slashes = 0; (character = fgetc(head)) != EOF && character != '\n';)
-    {
-        if (slashes == 2)
-        {
-            putchar(character);
-        }
-        else if (character == '/')
-        {
-            ++slashes;
-        }
-    }
-    printf("%%F{3})»");
-    fclose(head);
+	char *path;
+	FILE *head;
+	int character;
+	int totalOfSlashes = 0;
+	if (!root)
+	{
+		return;
+	}
+	path = malloc(length + 11);
+	sprintf(path, "%s/.git/HEAD", root);
+	head = fopen(path, "r");
+	free(path);
+	if (!head)
+	{
+		return;
+	}
+	printf("%%F{3}:«(%%f");
+	while ((character = fgetc(head)) != EOF && character != '\n')
+	{
+		if (totalOfSlashes == 2)
+		{
+			putchar(character);
+		}
+		else if (character == '/')
+		{
+			++totalOfSlashes;
+		}
+	}
+	printf("%%F{3})»");
+	fclose(head);
 }
 
-static void WriteCalendar(struct tm* date)
+static void writeCalendar(struct tm *date)
 {
-    char buffer[13];
-    strftime(buffer, sizeof(buffer), "(%a) %b %d", date);
-    printf("%%F{red}󰃭 %%f%s%s  ", buffer,
-           IS_ORDINAL_DAY(1)   ? "st"
-           : IS_ORDINAL_DAY(2) ? "nd"
-           : IS_ORDINAL_DAY(3) ? "rd"
-                               : "th");
+	char buffer[13];
+	strftime(buffer, sizeof(buffer), "(%a) %b %d", date);
+	printf("%%F{red}󰃭 %%f%s%s  ", buffer,
+		   IS_ORDINAL_DAY(1)   ? "st"
+		   : IS_ORDINAL_DAY(2) ? "nd"
+		   : IS_ORDINAL_DAY(3) ? "rd"
+							   : "th");
 }
 
-static void WriteClock(struct tm* date)
+static void writeClock(struct tm *date)
 {
-    printf("%s%%f%02dh%02dm",
-           date->tm_hour < 6    ? "%F{cyan}󰭎 "
-           : date->tm_hour < 12 ? "%F{red}󰖨 "
-           : date->tm_hour < 18 ? "%F{blue} "
-                                : "%F{yellow}󰽥 ",
-           date->tm_hour, date->tm_min);
+	printf("%s%%f%02dh%02dm",
+		   date->tm_hour < 6    ? "%F{cyan}󰭎 "
+		   : date->tm_hour < 12 ? "%F{red}󰖨 "
+		   : date->tm_hour < 18 ? "%F{blue} "
+								: "%F{yellow}󰽥 ",
+		   date->tm_hour, date->tm_min);
 }
 
-static void WriteCommandsSeparator(struct winsize* windowSize)
+static void writeCommandsSeparator(struct winsize *windowSize)
 {
-    for (int column = 0; column < windowSize->ws_col; ++column)
-    {
-        printf(column % 2 ? "%%F{red}v" : "%%F{yellow}≥");
-    }
-    printf("%%F{yellow}:«(");
+	int column;
+	for (column = 0; column < windowSize->ws_col; ++column)
+	{
+		printf(column % 2 ? "%%F{red}v" : "%%F{yellow}≥");
+	}
+	printf("%%F{yellow}:«(");
 }
 
-static void WriteDirectoryAccess(void)
+static void writeDirectoryAccess(void)
 {
-    printf(access(".", W_OK) ? "%%F{magenta} " : "");
+	printf(access(".", W_OK) ? "%%F{magenta} " : "");
 }
 
-static void WriteDiskUsage(void)
+static void writeDiskUsage(void)
 {
-    struct statvfs64 status;
-    statvfs64("/", &status);
-    fsblkcnt64_t totalBytes = status.f_frsize * status.f_blocks;
-    fsblkcnt64_t availableBytes = status.f_frsize * status.f_bavail;
-    int usage = ((totalBytes - availableBytes) / (float)totalBytes) * 100;
-    printf("%%F{%s}󰋊 %%f%d%%%%  ", usage < 70 ? "green" : usage < 80 ? "yellow" : "red", usage);
-    g_modulesLength += CountDigits(usage);
+	struct statvfs64 status;
+	fsblkcnt64_t totalBytes;
+	fsblkcnt64_t availableBytes;
+	int usage;
+	statvfs64("/", &status);
+	totalBytes = status.f_frsize * status.f_blocks;
+	availableBytes = status.f_frsize * status.f_bavail;
+	usage = ((totalBytes - availableBytes) / (float)totalBytes) * 100;
+	printf("%%F{%s}󰋊 %%f%d%%%%  ",
+		   usage < 70   ? "green"
+		   : usage < 80 ? "yellow"
+						: "red",
+		   usage);
+	g_modulesLength += countDigits(usage);
 }
 
-static void WriteExitCode(void)
+static void writeExitCode(void)
 {
-    printf("{%%(?..%%F{1})%%?%%F{3}}⤐  ");
+	printf("{%%(?..%%F{1})%%?%%F{3}}⤐  ");
 }
 
-static void WriteLocalIPV4Address(void)
+static void writeLocalIPV4Address(void)
 {
-    struct ifaddrs* interfacesList;
-    getifaddrs(&interfacesList);
-    char buffer[16] = "127.0.0.1";
-    for (struct ifaddrs* interface = interfacesList; interface; interface = interface->ifa_next)
-    {
-        if (interface->ifa_addr && interface->ifa_addr->sa_family & AF_INET && interface->ifa_flags & IFF_RUNNING &&
-            !(interface->ifa_flags & IFF_LOOPBACK))
-        {
-            inet_ntop(AF_INET, &((struct sockaddr_in*)interface->ifa_addr)->sin_addr, buffer, sizeof(buffer));
-            break;
-        }
-    }
-    freeifaddrs(interfacesList);
-    printf("%%F{blue} %%f%s  ", buffer);
-    g_modulesLength += strlen(buffer);
+	char buffer[16] = "127.0.0.1";
+	struct ifaddrs *interfacesList;
+	struct ifaddrs *interface;
+	getifaddrs(&interfacesList);
+	for (interface = interfacesList; interface; interface = interface->ifa_next)
+	{
+		if (interface->ifa_addr && interface->ifa_addr->sa_family & AF_INET &&
+			interface->ifa_flags & IFF_RUNNING && !(interface->ifa_flags & IFF_LOOPBACK))
+		{
+			inet_ntop(AF_INET, &((struct sockaddr_in *)interface->ifa_addr)->sin_addr,
+					  buffer, sizeof(buffer));
+			break;
+		}
+	}
+	freeifaddrs(interfacesList);
+	printf("%%F{blue} %%f%s  ", buffer);
+	g_modulesLength += strlen(buffer);
 }
 
-static void WriteModulesSeparator(struct winsize* windowSize)
+static void writeModulesSeparator(struct winsize *windowSize)
 {
-    printf("%%F{yellow})»:");
-    for (int column = 0; column < windowSize->ws_col - g_modulesLength; ++column)
-    {
-        printf(column % 2 ? "%%F{red}-" : "%%F{yellow}=");
-    }
+	int column;
+	printf("%%F{yellow})»:");
+	for (column = 0; column < windowSize->ws_col - g_modulesLength; ++column)
+	{
+		printf(column % 2 ? "%%F{red}-" : "%%F{yellow}=");
+	}
 }
 
-static void WritePath(const char* pwd, const char* gitRoot, size_t gitRootLength)
+static void writePath(const char *pwd, const char *gitRoot, size_t gitRootLength)
 {
-    !gitRoot || !gitRoot[1] ? printf("%%F{1}%%~")
-                            : printf("%%F{1}@/%s", pwd + FindLastSlash(gitRoot, gitRootLength) + 1);
+	!gitRoot || !gitRoot[1]
+		? printf("%%F{1}%%~")
+		: printf("%%F{1}@/%s", pwd + findLastSlash(gitRoot, gitRootLength) + 1);
 }
 
-static void WriteRootUserStatus(void)
+static void writeRootUserStatus(void)
 {
-    printf("%%F{3}%%(#.{%%F{1}#%%F{3}}.)");
+	printf("%%F{3}%%(#.{%%F{1}#%%F{3}}.)");
 }
 
-static void WriteVirtualEnvironment(void)
+static void writeVirtualEnvironment(void)
 {
-    const char* path = getenv("VIRTUAL_ENV");
-    if (path)
-    {
-        printf("%%f(%s) ", path + FindLastSlash(path, strlen(path)) + 1);
-    }
+	const char *path = getenv("VIRTUAL_ENV");
+	if (path)
+	{
+		printf("%%f(%s) ", path + findLastSlash(path, strlen(path)) + 1);
+	}
 }
 
 int main(void)
 {
-    const char* pwd = getenv("PWD");
-    char* gitRoot;
-    size_t gitRootLength;
-    time_t epoch = time(NULL);
-    struct tm* date = localtime(&epoch);
-    struct winsize windowSize;
-    ioctl(STDERR_FILENO, TIOCGWINSZ, &windowSize);
-    FindGitRoot(pwd, &gitRoot, &gitRootLength);
-    WriteCommandsSeparator(&windowSize);
-    WriteLocalIPV4Address();
-    WriteDiskUsage();
-    WriteBatteryCharge();
-    WriteCalendar(date);
-    WriteClock(date);
-    WriteModulesSeparator(&windowSize);
-    WriteRootUserStatus();
-    WriteExitCode();
-    WriteVirtualEnvironment();
-    WritePath(pwd, gitRoot, gitRootLength);
-    WriteGitBranch(gitRoot, gitRootLength);
-    WriteDirectoryAccess();
-    printf(" %%f\n");
-    if (gitRoot)
-    {
-        free(gitRoot);
-    }
-    return 0;
+	const char *pwd = getenv("PWD");
+	char *gitRoot;
+	size_t gitRootLength;
+	time_t epoch = time(NULL);
+	struct tm *date = localtime(&epoch);
+	struct winsize windowSize;
+	ioctl(STDERR_FILENO, TIOCGWINSZ, &windowSize);
+	findGitRoot(pwd, &gitRoot, &gitRootLength);
+	writeCommandsSeparator(&windowSize);
+	writeLocalIPV4Address();
+	writeDiskUsage();
+	writeBatteryCharge();
+	writeCalendar(date);
+	writeClock(date);
+	writeModulesSeparator(&windowSize);
+	writeRootUserStatus();
+	writeExitCode();
+	writeVirtualEnvironment();
+	writePath(pwd, gitRoot, gitRootLength);
+	writeGitBranch(gitRoot, gitRootLength);
+	writeDirectoryAccess();
+	printf(" %%f\n");
+	if (gitRoot)
+	{
+		free(gitRoot);
+	}
+	return (0);
 }
--- src/right-prompt.c
+++ src/right-prompt.c
@@ -5,109 +5,113 @@
 #include <sys/syscall.h>
 #include <unistd.h>
 
-#define WRITE_ENTRY_TYPE(a_symbol, a_color, a_value)                                                                   \
-    if (a_value)                                                                                                       \
-    {                                                                                                                  \
-        printf(" %%F{%s}%s%%f%zu", a_color, a_symbol, a_value);                                                        \
-    }
+#define WRITE_ENTRY_TYPE(a_symbol, a_color, a_value) \
+	if (a_value) \
+	{ \
+		printf(" %%F{%s}%s%%f%zu", a_color, a_symbol, a_value); \
+	}
 
 struct EntryTypes
 {
-    size_t regulars;
-    size_t directories;
-    size_t blocks;
-    size_t characters;
-    size_t sockets;
-    size_t fifos;
-    size_t symlinks;
+	size_t regulars;
+	size_t directories;
+	size_t blocks;
+	size_t characters;
+	size_t sockets;
+	size_t fifos;
+	size_t symlinks;
 };
 
 struct linux_dirent64
 {
-    ino64_t d_ino;
-    off64_t d_off;
-    unsigned short int d_reclen;
-    unsigned char d_type;
-    char d_name[];
+	ino64_t d_ino;
+	off64_t d_off;
+	unsigned short int d_reclen;
+	unsigned char d_type;
+	char d_name[];
 };
 
-static void CountEntryTypes(struct EntryTypes* types);
-static void WriteEntryTypes(void);
-static void WriteJobs(void);
+static void countEntryTypes(struct EntryTypes *types);
+static void writeEntryTypes(void);
+static void writeJobs(void);
 
-static void CountEntryTypes(struct EntryTypes* types)
+static void countEntryTypes(struct EntryTypes *types)
 {
-    int directory = open(".", O_RDONLY);
-    if (directory < 0)
-    {
-        return;
-    }
-    char buffer[1024];
-    struct linux_dirent64* entry;
-    for (long totalOfEntries; (totalOfEntries = syscall(SYS_getdents64, directory, buffer, sizeof(buffer))) > 0;)
-    {
-        for (long index = 0; index < totalOfEntries; index += entry->d_reclen)
-        {
-            entry = (struct linux_dirent64*)(buffer + index);
-            if (*entry->d_name == '.' && (!entry->d_name[1] || (entry->d_name[1] == '.' && !entry->d_name[2])))
-            {
-                continue;
-            }
-            if (entry->d_type == DT_REG)
-            {
-                ++types->regulars;
-            }
-            else if (entry->d_type == DT_DIR)
-            {
-                ++types->directories;
-            }
-            else if (entry->d_type == DT_BLK)
-            {
-                ++types->blocks;
-            }
-            else if (entry->d_type == DT_CHR)
-            {
-                ++types->characters;
-            }
-            else if (entry->d_type == DT_SOCK)
-            {
-                ++types->sockets;
-            }
-            else if (entry->d_type == DT_FIFO)
-            {
-                ++types->fifos;
-            }
-            else if (entry->d_type == DT_LNK)
-            {
-                ++types->symlinks;
-            }
-        }
-        close(directory);
-    }
+	char buffer[1024];
+	struct linux_dirent64 *entry;
+	long totalOfEntries;
+	long index;
+	int directory = open(".", O_RDONLY);
+	if (directory < 0)
+	{
+		return;
+	}
+	while ((totalOfEntries = syscall(SYS_getdents64, directory, buffer, sizeof(buffer))) >
+		   0)
+	{
+		for (index = 0; index < totalOfEntries; index += entry->d_reclen)
+		{
+			entry = (struct linux_dirent64 *)(buffer + index);
+			if (*entry->d_name == '.' &&
+				(!entry->d_name[1] || (entry->d_name[1] == '.' && !entry->d_name[2])))
+			{
+				continue;
+			}
+			if (entry->d_type == DT_REG)
+			{
+				++types->regulars;
+			}
+			else if (entry->d_type == DT_DIR)
+			{
+				++types->directories;
+			}
+			else if (entry->d_type == DT_BLK)
+			{
+				++types->blocks;
+			}
+			else if (entry->d_type == DT_CHR)
+			{
+				++types->characters;
+			}
+			else if (entry->d_type == DT_SOCK)
+			{
+				++types->sockets;
+			}
+			else if (entry->d_type == DT_FIFO)
+			{
+				++types->fifos;
+			}
+			else if (entry->d_type == DT_LNK)
+			{
+				++types->symlinks;
+			}
+		}
+		close(directory);
+	}
 }
 
-static void WriteEntryTypes(void)
+static void writeEntryTypes(void)
 {
-    struct EntryTypes types = {0, 0, 0, 0, 0, 0, 0};
-    CountEntryTypes(&types);
-    WRITE_ENTRY_TYPE(" ", "blue", types.regulars);
-    WRITE_ENTRY_TYPE(" ", "yellow", types.directories);
-    WRITE_ENTRY_TYPE("󰇖 ", "magenta", types.blocks);
-    WRITE_ENTRY_TYPE("󱣴 ", "green", types.characters);
-    WRITE_ENTRY_TYPE("󱄙 ", "cyan", types.sockets);
-    WRITE_ENTRY_TYPE("󰟦 ", "blue", types.fifos);
-    WRITE_ENTRY_TYPE("󰌷 ", "blue", types.symlinks);
+	struct EntryTypes types = {0, 0, 0, 0, 0, 0, 0};
+	countEntryTypes(&types);
+	WRITE_ENTRY_TYPE(" ", "blue", types.regulars);
+	WRITE_ENTRY_TYPE(" ", "yellow", types.directories);
+	WRITE_ENTRY_TYPE("󰇖 ", "magenta", types.blocks);
+	WRITE_ENTRY_TYPE("󱣴 ", "green", types.characters);
+	WRITE_ENTRY_TYPE("󱄙 ", "cyan", types.sockets);
+	WRITE_ENTRY_TYPE("󰟦 ", "blue", types.fifos);
+	WRITE_ENTRY_TYPE("󰌷 ", "blue", types.symlinks);
 }
 
-static void WriteJobs(void)
+static void writeJobs(void)
 {
-    printf("%%(1j. %%F{magenta} %%f%%j.)");
+	printf("%%(1j. %%F{magenta} %%f%%j.)");
 }
 
 int main(void)
 {
-    WriteEntryTypes();
-    WriteJobs();
-    putchar('\n');
-    return 0;
+	writeEntryTypes();
+	writeJobs();
+	putchar('\n');
+	return (0);
 }
