87945e5892107f17780fb8140cf04c4d71f34eaf
Sherman Rofeman <skippyr.developer@gmail.com>
2024-03-23 20:50:34 -0300

    update format for C

--- src/right-prompt.h
+++ src/right-prompt.h
@@ -4,13 +4,13 @@
 #include <string.h>
 
 struct EntryTypesCount {
-  size_t regulars;
-  size_t directories;
-  size_t blocks;
-  size_t characters;
-  size_t sockets;
-  size_t fifos;
-  size_t symlinks;
+    size_t regulars;
+    size_t directories;
+    size_t blocks;
+    size_t characters;
+    size_t sockets;
+    size_t fifos;
+    size_t symlinks;
 };
 
 static void countEntryTypes(struct EntryTypesCount *count);
