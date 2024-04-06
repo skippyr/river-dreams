7a3b1972f00510a7cdd8ced554a6c1cc73bba0e4
Sherman Rofeman <skippyr.developer@gmail.com>
2024-04-06 15:08:35 -0300

    update formatting

--- src/right-prompt.c
+++ src/right-prompt.c
@@ -1,62 +1,69 @@
 #include "right-prompt.h"
 
-static void countEntryTypes(struct EntryTypesCount *count) {
-  memset(count, 0, sizeof(struct EntryTypesCount));
-  DIR *directory = opendir(".");
-  if (!directory) {
-    return;
-  }
-  for (struct dirent *entry; (entry = readdir(directory));) {
-    if (*entry->d_name == '.' &&
-        (!entry->d_name[1] || (entry->d_name[1] == '.' && !entry->d_name[2]))) {
-      continue;
+static void countEntryTypes(struct EntryTypesCount* count)
+{
+    memset(count, 0, sizeof(struct EntryTypesCount));
+    DIR* directory = opendir(".");
+    if (!directory)
+    {
+        return;
     }
-    switch (entry->d_type) {
-    case DT_REG:
-      ++count->regulars;
-      break;
-    case DT_DIR:
-      ++count->directories;
-      break;
-    case DT_BLK:
-      ++count->blocks;
-      break;
-    case DT_CHR:
-      ++count->characters;
-      break;
-    case DT_SOCK:
-      ++count->sockets;
-      break;
-    case DT_FIFO:
-      ++count->fifos;
-      break;
-    case DT_LNK:
-      ++count->symlinks;
-      break;
+    for (struct dirent* entry; (entry = readdir(directory));)
+    {
+        if (*entry->d_name == '.' && (!entry->d_name[1] || (entry->d_name[1] == '.' && !entry->d_name[2])))
+        {
+            continue;
+        }
+        switch (entry->d_type)
+        {
+        case DT_REG:
+            ++count->regulars;
+            break;
+        case DT_DIR:
+            ++count->directories;
+            break;
+        case DT_BLK:
+            ++count->blocks;
+            break;
+        case DT_CHR:
+            ++count->characters;
+            break;
+        case DT_SOCK:
+            ++count->sockets;
+            break;
+        case DT_FIFO:
+            ++count->fifos;
+            break;
+        case DT_LNK:
+            ++count->symlinks;
+            break;
+        }
     }
-  }
-  closedir(directory);
+    closedir(directory);
 }
 
-static void writeEntryTypesCountModule(void) {
-  struct EntryTypesCount count;
-  countEntryTypes(&count);
-  WRITE_ENTRY_TYPE_COUNT((char *)NULL, " ", count.regulars);
-  WRITE_ENTRY_TYPE_COUNT("yellow", " ", count.directories);
-  WRITE_ENTRY_TYPE_COUNT("magenta", "󰇖 ", count.blocks);
-  WRITE_ENTRY_TYPE_COUNT("green", "󱣴 ", count.characters);
-  WRITE_ENTRY_TYPE_COUNT("cyan", "󱄙 ", count.sockets);
-  WRITE_ENTRY_TYPE_COUNT("blue", "󰟦 ", count.fifos);
-  WRITE_ENTRY_TYPE_COUNT("blue", "󰌷 ", count.symlinks);
+static void writeEntryTypesCountModule(void)
+{
+    struct EntryTypesCount count;
+    countEntryTypes(&count);
+    WRITE_ENTRY_TYPE_COUNT((char*)NULL, " ", count.regulars);
+    WRITE_ENTRY_TYPE_COUNT("yellow", " ", count.directories);
+    WRITE_ENTRY_TYPE_COUNT("magenta", "󰇖 ", count.blocks);
+    WRITE_ENTRY_TYPE_COUNT("green", "󱣴 ", count.characters);
+    WRITE_ENTRY_TYPE_COUNT("cyan", "󱄙 ", count.sockets);
+    WRITE_ENTRY_TYPE_COUNT("blue", "󰟦 ", count.fifos);
+    WRITE_ENTRY_TYPE_COUNT("blue", "󰌷 ", count.symlinks);
 }
 
-static void writeJobsCountModule(void) {
-  printf("%%(1j. %%F{magenta} %%f%%j.)");
+static void writeJobsCountModule(void)
+{
+    printf("%%(1j. %%F{magenta} %%f%%j.)");
 }
 
-int main(void) {
-  writeEntryTypesCountModule();
-  writeJobsCountModule();
-  putchar('\n');
-  return 0;
+int main(void)
+{
+    writeEntryTypesCountModule();
+    writeJobsCountModule();
+    putchar('\n');
+    return 0;
 }
--- src/right-prompt.h
+++ src/right-prompt.h
@@ -4,25 +4,30 @@
 #include <stdio.h>
 #include <string.h>
 
-#define WRITE_ENTRY_TYPE_COUNT(color_a, symbol_a, value_a) \
-  if (value_a) { \
-    if (color_a) { \
-      printf(" %%F{%s}%s%%f%zu", color_a, symbol_a, value_a); \
-    } else { \
-      printf("%s%zu", symbol_a, value_a); \
-    } \
-  }
+#define WRITE_ENTRY_TYPE_COUNT(a_color, a_symbol, a_value) \
+    if (a_value) \
+    { \
+        if (a_color) \
+        { \
+            printf(" %%F{%s}%s%%f%zu", a_color, a_symbol, a_value); \
+        } \
+        else \
+        { \
+            printf("%s%zu", a_symbol, a_value); \
+        } \
+    }
 
-struct EntryTypesCount {
-  size_t regulars;
-  size_t directories;
-  size_t blocks;
-  size_t characters;
-  size_t sockets;
-  size_t fifos;
-  size_t symlinks;
+struct EntryTypesCount
+{
+    size_t regulars;
+    size_t directories;
+    size_t blocks;
+    size_t characters;
+    size_t sockets;
+    size_t fifos;
+    size_t symlinks;
 };
 
-static void countEntryTypes(struct EntryTypesCount *count);
+static void countEntryTypes(struct EntryTypesCount* count);
 static void writeEntryTypesCountModule(void);
 static void writeJobsCountModule(void);
