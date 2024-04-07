d9c84c966538fb901c65c1fd42c72eed3573c72a
Sherman Rofeman <skippyr.developer@gmail.com>
2024-04-07 14:39:01 -0300

    update .clang-format

--- src/right-prompt.h
+++ src/right-prompt.h
@@ -5,27 +5,22 @@
 #include <string.h>
 
 #define WRITE_ENTRY_TYPE_COUNT(a_color, a_symbol, a_value) \
-    if (a_value) \
-    { \
-        if (a_color) \
-        { \
-            printf(" %%F{%s}%s%%f%zu", a_color, a_symbol, a_value); \
-        } \
-        else \
-        { \
-            printf("%s%zu", a_symbol, a_value); \
-        } \
-    }
+  if (a_value) { \
+    if (a_color) { \
+      printf(" %%F{%s}%s%%f%zu", a_color, a_symbol, a_value); \
+    } else { \
+      printf("%s%zu", a_symbol, a_value); \
+    } \
+  }
 
-struct EntryTypesCount
-{
-    size_t regulars;
-    size_t directories;
-    size_t blocks;
-    size_t characters;
-    size_t sockets;
-    size_t fifos;
-    size_t symlinks;
+struct EntryTypesCount {
+  size_t regulars;
+  size_t directories;
+  size_t blocks;
+  size_t characters;
+  size_t sockets;
+  size_t fifos;
+  size_t symlinks;
 };
 
 static void countEntryTypes(struct EntryTypesCount* count);
