05f07780fad741a23d16e34c13a01f50dcd2d891
Sherman Rofeman <skippyr.developer@icloud.com>
2024-10-09 12:17:11 -0300

    Make common be lib

---
+++ src/common.c
@@ -0,0 +1,9 @@
+#include "common.h"
+
+int countDigits(int number) {
+  int totalDigits;
+  for (totalDigits = !number; number; number /= 10) {
+    ++totalDigits;
+  }
+  return totalDigits;
+}
--- src/common.h
+++ src/common.h
@@ -1,7 +1,1 @@
-static int countDigits(int number) {
-  int totalDigits;
-  for (totalDigits = !number; number; number /= 10) {
-    ++totalDigits;
-  }
-  return totalDigits;
-}
+int countDigits(int number);
